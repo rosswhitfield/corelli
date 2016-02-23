8af745f0076f38a0556f35c1dd4a2bff563f0d9f
Ross Whitfield <whitfieldre@ornl.gov>
2016-02-23 12:17:05 -0500

    cron update

--- cal_2016_02/AlignC60_2.py
+++ cal_2016_02/AlignC60_2.py
@@ -12,12 +12,12 @@ componentList1=componentList1[:-1]
 print componentList1
 
 # rows - Y
-AlignComponents(CalibrationTable="C60_cal",MaskWorkspace="C60_mask",InstrumentFilename="/SNS/users/rwp/CORELLI_Definition_88.14cm.xml",ComponentList="A row,B row,C row",Yposition=True)
+#AlignComponents(CalibrationTable="C60_cal",MaskWorkspace="C60_mask",InstrumentFilename="/SNS/users/rwp/CORELLI_Definition_88.14cm.xml",ComponentList="A row,B row,C row",Yposition=True)
 print mtd['alignedWorkspace'].getInstrument().getComponentByName('A row').getPos()
 print mtd['alignedWorkspace'].getInstrument().getComponentByName('B row').getPos()
 print mtd['alignedWorkspace'].getInstrument().getComponentByName('C row').getPos()
 
 
-AlignComponents(CalibrationTable="C60_cal",MaskWorkspace="C60_mask",Workspace='alignedWorkspace',EulerConvention='YXZ',ComponentList=componentList1,Xposition=True,Zposition=True,AlphaRotation=True,BetaRotation=True,GammaRotation=True,MinAlphaRotation=-20,MaxAlphaRotation=20)
+AlignComponents(CalibrationTable="C60_cal",MaskWorkspace="C60_mask",InstrumentFilename="/SNS/users/rwp/CORELLI_Definition_88.14cm.xml",EulerConvention='YXZ',ComponentList=componentList1,Xposition=True,Zposition=True,AlphaRotation=True,BetaRotation=True,GammaRotation=True,MinAlphaRotation=-20,MaxAlphaRotation=20)
 ExportGeometry(InputWorkspace='alignedWorkspace',Filename='/SNS/users/rwp/corelli/cal_2016_02/AlignC60_2.xml',EulerConvention='YXZ',Components=componentList1)
 SaveNexus(InputWorkspace='alignedWorkspace',Filename='/SNS/users/rwp/corelli/cal_2016_02/AlignC60_2.nxs')
