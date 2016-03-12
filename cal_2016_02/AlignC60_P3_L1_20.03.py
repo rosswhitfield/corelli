f0c9d0644de191b477e953092f3ae214aa7c41d7
Ross Whitfield <whitfieldre@ornl.gov>
2016-03-11 23:17:04 -0500

    cron update

--- cal_2016_02/AlignC60_P3_L1_20.03.py
+++ cal_2016_02/AlignC60_P3_L1_20.03.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python2
 from mantid.simpleapi import *
 
-LoadCalFile(InstrumentFilename='/SNS/users/rwp/CORELLI_Definition_88.14cm.xml', CalFilename='/SNS/users/rwp/corelli/cal_2016_02/cal_C60_20501-8_sum4_mask_lt_3.cal', WorkspaceName='C60')
+LoadCalFile(InstrumentFilename='/SNS/users/rwp/CORELLI_Definition_88.14cm.xml', CalFilename='/SNS/users/rwp/corelli/cal_2016_02/cal_C60_20501-8_sum4_mask_lt_3_L1_20.03.cal', WorkspaceName='C60')
 MaskBTP(Workspace='C60_mask',Pixel="1-16,241-256")
 MaskBTP(Workspace='C60_mask',Bank="15,16,19,20,21,47,51,52,77,78,81,82,83")
 
@@ -22,5 +22,5 @@ print mtd['alignedWorkspace'].getInstrument().getComponentByName('C row').getPos
 
 
 AlignComponents(CalibrationTable="C60_cal",MaskWorkspace="C60_mask",Workspace='alignedWorkspace',EulerConvention='YXZ',ComponentList=componentList1,Xposition=True,Zposition=True,AlphaRotation=True,BetaRotation=True,GammaRotation=True,MinAlphaRotation=-20,MaxAlphaRotation=20)
-ExportGeometry(InputWorkspace='alignedWorkspace',Filename='/SNS/users/rwp/corelli/cal_2016_02/AlignC60_P3_L1_20.03.xml',EulerConvention='YXZ',Components=componentList1)
-SaveNexus(InputWorkspace='alignedWorkspace',Filename='/SNS/users/rwp/corelli/cal_2016_02/AlignC60_P3_L1_20.03.nxs')
+ExportGeometry(InputWorkspace='alignedWorkspace',Filename='/SNS/users/rwp/corelli/cal_2016_02/AlignC60_P3_L1_20.03_2.xml',EulerConvention='YXZ',Components=componentList1)
+SaveNexus(InputWorkspace='alignedWorkspace',Filename='/SNS/users/rwp/corelli/cal_2016_02/AlignC60_P3_L1_20.03_2.nxs')
--- cal_2016_02/AlignSi_P3_L1_20.03.py
+++ cal_2016_02/AlignSi_P3_L1_20.03.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python2
 from mantid.simpleapi import *
 
-LoadCalFile(InstrumentFilename='/SNS/users/rwp/CORELLI_Definition_88.14cm.xml', CalFilename='/SNS/users/rwp/corelli/cal_2016_02/cal_Si_20492-9_sum4_mask_lt_3.cal', WorkspaceName='Si')
+LoadCalFile(InstrumentFilename='/SNS/users/rwp/CORELLI_Definition_88.14cm.xml', CalFilename='/SNS/users/rwp/corelli/cal_2016_02/cal_Si_20492-9_sum4_mask_lt_3_L1_20.03.cal', WorkspaceName='Si')
 MaskBTP(Workspace='Si_mask',Pixel="1-16,241-256")
 
 componentList1=""
@@ -21,5 +21,5 @@ print mtd['alignedWorkspace'].getInstrument().getComponentByName('C row').getPos
 
 
 AlignComponents(CalibrationTable="Si_cal",MaskWorkspace="Si_mask",Workspace='alignedWorkspace',EulerConvention='YXZ',ComponentList=componentList1,Xposition=True,Zposition=True,AlphaRotation=True,BetaRotation=True,GammaRotation=True,MinAlphaRotation=-20,MaxAlphaRotation=20)
-ExportGeometry(InputWorkspace='alignedWorkspace',Filename='/SNS/users/rwp/corelli/cal_2016_02/AlignSi_P3_L1_20.03.xml',EulerConvention='YXZ',Components=componentList1)
-SaveNexus(InputWorkspace='alignedWorkspace',Filename='/SNS/users/rwp/corelli/cal_2016_02/AlignSi_P3_L1_20.03.nxs')
+ExportGeometry(InputWorkspace='alignedWorkspace',Filename='/SNS/users/rwp/corelli/cal_2016_02/AlignSi_P3_L1_20.03_2.xml',EulerConvention='YXZ',Components=componentList1)
+SaveNexus(InputWorkspace='alignedWorkspace',Filename='/SNS/users/rwp/corelli/cal_2016_02/AlignSi_P3_L1_20.03_2.nxs')
