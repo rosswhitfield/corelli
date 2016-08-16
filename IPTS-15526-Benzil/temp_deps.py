83f3eaa44988846b27322d5ba71d3d3d0a19d916
Ross Whitfield <whitfieldre@ornl.gov>
2016-08-16 12:17:06 -0400

    cron update

--- IPTS-15526-Benzil/temp_deps.py
+++ IPTS-15526-Benzil/temp_deps.py
@@ -11,5 +11,7 @@ for r in runs:
         dataR=CropWorkspace(dataR,XMin=2.5,XMax=10)
         SetGoniometer(dataR,Axis0="BL9:Mot:Sample:Axis1,0,1,0,1") 
         LoadIsawUB(InputWorkspace=dataR,Filename="/SNS/users/rwp/benzil/benzil_Hexagonal.mat")
-        ConvertToMD(InputWorkspace=dataR,OutputWorkspace=name,QDimensions='Q3D',dEAnalysisMode='Elastic', Q3DFrames='HKL',
-                       QConversionScales='HKL',LorentzCorrection='0',Uproj='1,1,0',Vproj='1,-1,0',Wproj='0,0,1',MinValues='-10.1,-10.1,-5.1',MaxValues='10.1,10.1,5.1')
+        ConvertToMD(InputWorkspace=dataR,OutputWorkspace='results',QDimensions='Q3D',dEAnalysisMode='Elastic', Q3DFrames='HKL',OtherDimensions='BL9:SE:Lakeshore:SETP2',
+                       QConversionScales='HKL',LorentzCorrection='0',Uproj='1,1,0',Vproj='1,-1,0',Wproj='0,0,1',MinValues='-1.1,-10.1,-5.1,50',MaxValues='5.1,0,5.1,300',
+                       OverwriteExisting=0)
+
