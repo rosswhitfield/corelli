1afb5b4313176e2b22fdd8f49dfc4157075e6ca0
Ross Whitfield <whitfieldre@ornl.gov>
2016-08-22 12:17:08 -0400

    cron update

--- cross_correlate_test2/plot_bank.py
+++ cross_correlate_test2/plot_bank.py
@@ -2,6 +2,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+fig = plt.figure(figsize=(12,6))
 total = np.zeros((341,1667))
 for bank in range(31,62):
     results = np.zeros((341,1667))
@@ -14,20 +15,20 @@ for bank in range(31,62):
     xx=np.array(range(x))*10
     yy=np.array(range(y))*10
     X,Y = np.meshgrid(xx,yy)
-    fig = plt.figure(figsize=(12,6))
+    plt.clf()
     plt.pcolormesh(X, Y, results, vmin=0, vmax=2000)
     #plt.pcolormesh(X, Y, results)
     plt.colorbar()
-    plt.title('bank'+str(bank))
+    plt.title('PrAl3 bank'+str(bank))
     plt.xlabel('total time of flight (uS)')
     plt.ylabel('incident fliught time (uS)')
     plt.savefig('results_events_bank'+str(bank)+'.png')
 
-fig = plt.figure(figsize=(12,6))
+plt.clf()
 plt.pcolormesh(X, Y, total, vmin=0, vmax=50000)
 #plt.pcolormesh(X, Y, total)
 plt.colorbar()
-plt.title('all banks')
+plt.title('PrAl3 all banks')
 plt.xlabel('total time of flight (uS)')
 plt.ylabel('incident fliught time (uS)')
 plt.savefig('results_events_all_bank.png')
