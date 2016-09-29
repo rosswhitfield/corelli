6218be7b6a7e5da2e24c03cb746ab0cfa342d262
Ross Whitfield <whitfieldre@ornl.gov>
2016-09-28 23:17:32 -0400

    cron update

---
+++ cross_correlate_test2/export_bank_e_1_r.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+import numpy as np
+
+total = np.zeros((341,16667))
+for bank in range(31,62):
+    results = np.zeros((341,16667))
+    for run in range(30338,30346):
+        filename = 'CORELLI_'+str(run)+'_results_events_b1_bank_r2.584_'+str(bank)+'.npy'
+        temp=np.load(filename)
+        results += temp
+        total += temp
+
+y,x = results.shape
+xx=np.array(range(x))*1
+        
+
+L1=20.0
+L2=2.583
+Lmc=17.999347
+chopper_per = 3408.28925864
+m=1.674927351e-27
+
+# 1D define Ei
+
+for mev, offset, x_offset in [(12,45,-0.05), (15,45,0.03), (20,45,-0.18), (25,40,-0.3), (30,40,-0.26), (50,50,0.03), (75,50,0.3)]:
+    print(mev,offset)    
+    ei = mev/1e3 * 1.602e-19
+    vi = np.sqrt(2*ei/m)
+    ti = Lmc/vi
+    y = ti*1e6%chopper_per
+    tof = (L1+L2)/vi
+    xi = int(tof*1e6)
+    t1 = L1/vi
+    ix = range(xi-1000,xi+1000)
+    t2 = xx[ix]/1e6 - t1
+    v2 = L2/t2
+    ef = 0.5*m*v2**2
+    dEs = (ef-ei)/1.602e-19*1000 - x_offset
+    ints = total[int((y-offset)/10),ix]
+    np.savetxt("Ei_"+str(mev)+"_meV.txt",np.array([dEs,ints]).T)
--- powder.py
+++ powder.py
@@ -16,6 +16,10 @@ SNSPowderReduction(Filename=runs,
                    SaveAs='fullprof',
                    OutputDirectory='/SNS/users/rwp/temp/')
 
+MergeRuns(InputWorkspaces='CORELLI_32307,CORELLI_32308,CORELLI_32309,CORELLI_32310,CORELLI_32311,CORELLI_32312,CORELLI_32313,CORELLI_32314,CORELLI_32315,CORELLI_32316,CORELLI_32317,CORELLI_32318,CORELLI_32319,CORELLI_32320,CORELLI_32321,CORELLI_32322,CORELLI_32323,CORELLI_32324,CORELLI_32325,CORELLI_32326,CORELLI_32327,CORELLI_32328', OutputWorkspace='sumed')
+
+
+"""
 SNSPowderReduction(Filename=runs,
                    Sum=True,
                    VanadiumNumber='28119',
@@ -23,3 +27,4 @@ SNSPowderReduction(Filename=runs,
                    Binning='0.1,-0.001,6',
                    SaveAs='fullprof',
                    OutputDirectory='/SNS/users/rwp/temp2/')
+"""
