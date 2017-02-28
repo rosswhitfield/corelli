0a824bb9d5a0d36e1e2007d3d743616adb4d8795
Ross Whitfield <whitfieldre@ornl.gov>
2017-02-27 23:17:35 -0500

    cron update

---
+++ tube_calibration/tc1.py
@@ -0,0 +1,19 @@
+import tube
+from tube_spec import TubeSpec
+import numpy as np
+run=39611 # 57
+ws = Load('CORELLI_'+str(run))
+ws = LoadInstrument(ws, Filename='/SNS/users/rwp/CORELLI_Definition_91.07cm.xml', RewriteSpectraMap='False')
+ws = Integration(ws)
+CloneWorkspace(InputWorkspace='ws', OutputWorkspace='ws2')
+
+tubeSet = TubeSpec(ws)
+tubeSet.setTubeSpecByString("bank57")
+
+a=(2*25.4+2)/1000
+knownPositions=np.arange(-7.5*a,8.5*a,a)
+funcForm = [1]*16
+
+calibTable = tube.calibrate(ws, tubeSet, knownPositions, funcForm,margin=4)
+
+ApplyCalibration(Workspace='ws2', PositionTable='CalibTable')
