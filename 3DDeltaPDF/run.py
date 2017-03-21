f0132b34f83847db2fd7f48ad3239036facd1127
Ross Whitfield <whitfieldre@ornl.gov>
2017-03-21 18:19:04 -0400

    cron update

--- 3DDeltaPDF/run.py
+++ 3DDeltaPDF/run.py
@@ -1,13 +1,11 @@
 from mantid.simpleapi import LoadMD, CreateMDHistoWorkspace
 from mantid.geometry import SpaceGroupFactory
-from astropy.convolution import convolve, Gaussian1DKernel
+from astropy.convolution import convolve, convolve_fft, Gaussian1DKernel
 import numpy as np
 import matplotlib.pyplot as plt
 
-
 filename='/SNS/CORELLI/IPTS-16344/shared/symm_007K_long_all_ub_13feb.nxs'
 space_group='F 2 3'
-
 
 ws=LoadMD(filename)
 
@@ -36,14 +34,26 @@ for d in range(ws.getNumDims()):
 
 signal=ws.getSignalArray().copy()
 
+# Do something with nan's and inf's
 signal[np.isnan(signal)]=0
 signal[np.isinf(signal)]=0
 
 fft=np.fft.fftshift(np.fft.fftn(np.fft.fftshift(signal)))
+out=np.real(fft*np.conj(fft))
+
+# convolve
+signal=ws.getSignalArray().copy()
+
+G1D = Gaussian1DKernel(2).array
+G3D = G1D * G1D.reshape((-1,1)) * G1D.reshape((-1,1,1))
+convolved = convolve(signal, G3D)
+
+convolved_fft=np.fft.fftshift(np.fft.fftn(np.fft.fftshift(convolved)))
+convolved_out=np.real(fft*np.conj(fft))
 
 # Create output workspace
-CreateMDHistoWorkspace(SignalInput=np.real(fft),
-                       ErrorInput=np.real(fft)**2,
+CreateMDHistoWorkspace(SignalInput=out,
+                       ErrorInput=out**2,
                        Dimensionality=3,
                        Extents=np.array(dim_list).flatten(),
                        NumberOfBins=signal.shape,
