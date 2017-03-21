b36cbc6b574e5f07512a07691f0d6cde85f46fad
Ross Whitfield <whitfieldre@ornl.gov>
2017-03-21 17:19:03 -0400

    cron update

--- 3DDeltaPDF/run.py
+++ 3DDeltaPDF/run.py
@@ -1,11 +1,12 @@
 from mantid.simpleapi import LoadMD, CreateMDHistoWorkspace
 from mantid.geometry import SpaceGroupFactory
-from astropy.convolution import convolve
+from astropy.convolution import convolve, Gaussian1DKernel
 import numpy as np
+import matplotlib.pyplot as plt
+
 
 filename='/SNS/CORELLI/IPTS-16344/shared/symm_007K_long_all_ub_13feb.nxs'
 space_group='F 2 3'
-
 
 
 ws=LoadMD(filename)
@@ -20,8 +21,12 @@ if not np.all(ws.getExperimentInfo(0).run().getLogData('W_MATRIX').value ==
 # Check centered on zero
 # Should also check center bin is 0
 dim_list=[]
+dim_names=['[H,0,0]','[0,K,0]','[0,0,L]']
 for d in range(ws.getNumDims()):
     dim = ws.getDimension(d)
+    if dim.name not in dim_names:
+        print("Dimensions must be one of ",dim_names)
+        exit()
     if dim.getMaximum() != -dim.getMinimum():
         print('Workspace dimensions must be centered on zero')
         exit()
@@ -29,13 +34,16 @@ for d in range(ws.getNumDims()):
     fft_dim=np.fft.fftshift(np.fft.fftfreq(dim.getNBins(), (dim.getMaximum()-dim.getMinimum())/dim.getNBins()))
     dim_list.append((fft_dim[0],fft_dim[-1]))
 
-signal=ws.getSignalArray()
+signal=ws.getSignalArray().copy()
 
+signal[np.isnan(signal)]=0
+signal[np.isinf(signal)]=0
 
+fft=np.fft.fftshift(np.fft.fftn(np.fft.fftshift(signal)))
 
 # Create output workspace
-CreateMDHistoWorkspace(SignalInput=np.zeros(signal.shape),
-                       ErrorInput=np.ones(signal.shape),
+CreateMDHistoWorkspace(SignalInput=np.real(fft),
+                       ErrorInput=np.real(fft)**2,
                        Dimensionality=3,
                        Extents=np.array(dim_list).flatten(),
                        NumberOfBins=signal.shape,
