6da6b2edf94e21b7688fbceb3a3f2f3ef7f9e345
Ross Whitfield <whitfieldre@ornl.gov>
2017-04-18 12:19:07 -0400

    cron update

--- IPTS-16617-PMN/fft.py
+++ IPTS-16617-PMN/fft.py
@@ -1,7 +1,9 @@
 LoadMD(Filename='/SNS/users/rwp/corelli/IPTS-16617-PMN/PMN_normdata_48sym_300K.nxs', LoadHistory=False, OutputWorkspace='pmn')
 
+q_max=10
+q_max=6.5
+q_max=4.8
+q_max=1.8
 
-DeltaPDF3D(InputWorkspace='pmn', IntermediateWorkspace='i_10', OutputWorkspace='fft_10', CropSphere=True, SphereMin='0.2', SphereMax='10')
-DeltaPDF3D(InputWorkspace='pmn', IntermediateWorkspace='i_6.5', OutputWorkspace='fft_6.5', CropSphere=True, SphereMin='0.2', SphereMax='6.5')
-DeltaPDF3D(InputWorkspace='pmn', IntermediateWorkspace='i_6.5', OutputWorkspace='fft_4.8', CropSphere=True, SphereMin='0.2', SphereMax='4.8')
-DeltaPDF3D(InputWorkspace='pmn', IntermediateWorkspace='i_1.8', OutputWorkspace='fft_1.8', CropSphere=True, SphereMin='0.2', SphereMax='1.2')
+DeltaPDF3D(InputWorkspace='pmn',  OutputWorkspace='fft', CropSphere=True, SphereMin='0.2', SphereMax=q_max)
+SaveMD('fft','/SNS/users/rwp/corelli/IPTS-16617-PMN/PMN_normdata_48sym_300K_fft_q_max'+str(q_max)+'.nxs')
