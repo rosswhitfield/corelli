fc252573bcfd7352757e7cc29b2dde1deb181889
Ross Whitfield <whitfieldre@ornl.gov>
2017-05-05 09:20:34 -0400

    cron update

--- tube_calibration/tc1.py
+++ tube_calibration/tc1.py
@@ -6,6 +6,7 @@ ws = Load('CORELLI_'+str(run))
 LoadInstrument(ws, Filename='/SNS/users/rwp/CORELLI_Definition_91.07cm.xml', RewriteSpectraMap='False')
 ws = Integration(ws)
 CloneWorkspace(InputWorkspace='ws', OutputWorkspace='ws2')
+CloneWorkspace(InputWorkspace='ws', OutputWorkspace='ws1')
 
 tubeSet = TubeSpec(ws)
 tubeSet.setTubeSpecByString("bank57")
@@ -14,6 +15,10 @@ a=(2*25.4+2)/1000
 knownPositions=np.arange(-7.5*a,8.5*a,a)
 funcForm = [1]*16
 
-calibTable = tube.calibrate(ws, tubeSet, knownPositions, funcForm,margin=6)
+calibTable = tube.calibrate(ws, tubeSet, knownPositions, funcForm,margin=6, plotTube=True, outputPeak=True)
 
 ApplyCalibration(Workspace='ws2', PositionTable='CalibTable')
+
+calibTable1 = tube.calibrate(ws, tubeSet, knownPositions, funcForm,margin=6, fitPolyn=1)
+
+ApplyCalibration(Workspace='ws1', PositionTable='CalibTable')
