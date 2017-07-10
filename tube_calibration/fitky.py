5b9c7ba22ec07a3f405799448edc51ef144e32a4
Ross Whitfield <whitfieldre@ornl.gov>
2017-07-10 15:19:07 -0400

    cron update

--- tube_calibration/fitky.py
+++ tube_calibration/fitky.py
@@ -6,16 +6,20 @@ import numpy as np
 
 dirc='/SNS/users/rwp/corelli/tube_calibration/'
 
-for bank in [23,56,85]:
-    COR_47299=LoadEventNexus('/SNS/CORELLI/IPTS-18479/nexus/CORELLI_47299.nxs.h5',BankName='bank'+str(bank))
-    COR_47299=Integration(COR_47299)
-    COR_47299_Y=COR_47299.extractY()*-1
-    for tube in range(8):
-        np.savetxt(dirc+'COR_47299_'+str(bank)+'_'+str(tube+1)+'.txt', np.concatenate((np.array(range(256),ndmin=2).T, COR_47299_Y[range(256*tube,256*(tube+1))]),axis=1))
+run=47299
 
-for bank in [33,45,57]:
-    COR_47299=LoadEventNexus('/SNS/CORELLI/IPTS-18479/nexus/CORELLI_47300.nxs.h5',BankName='bank'+str(bank))
-    COR_47299=Integration(COR_47299)
-    COR_47299_Y=COR_47299.extractY()*-1
-    for tube in range(8):
-        np.savetxt(dirc+'COR_47300_'+str(bank)+'_'+str(tube+1)+'.txt', np.concatenate((np.array(range(256),ndmin=2).T, COR_47299_Y[range(256*tube,256*(tube+1))]),axis=1))
+for bank in [23,56,85]:
+    data=LoadEventNexus('/SNS/CORELLI/IPTS-18479/nexus/CORELLI_'+str(run)+'.nxs.h5',BankName='bank'+str(bank))
+    data=Integration(data)
+    data_Y=data.extractY()*-1
+    for tube in range(16):
+        np.savetxt(dirc+'COR_'+str(run)+'_'+str(bank)+'_'+str(tube+1)+'.txt', np.concatenate((np.array(range(256),ndmin=2).T, data_Y[range(256*tube,256*(tube+1))]),axis=1))
+
+for run in range(47300,47305):
+    for bank in [33,45,57]:
+        data=LoadEventNexus('/SNS/CORELLI/IPTS-18479/nexus/CORELLI_'+str(run)+'.nxs.h5',BankName='bank'+str(bank))
+        data=Integration(data)
+        data_Y=data.extractY()*-1
+        for tube in range(16):
+            np.savetxt(dirc+'COR_'+str(run)+'_'+str(bank)+'_'+str(tube+1)+'.txt', np.concatenate((np.array(range(256),ndmin=2).T, data_Y[range(256*tube,256*(tube+1))]),axis=1))
+
