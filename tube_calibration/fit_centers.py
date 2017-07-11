26611bd8abb16c264e966690396d9bc46b1161d5
Ross Whitfield <whitfieldre@ornl.gov>
2017-07-11 13:19:05 -0400

    cron update

--- tube_calibration/fit_centers.py
+++ tube_calibration/fit_centers.py
@@ -2,6 +2,11 @@ import numpy as np
 
 centers=np.loadtxt('centers')
 
+a=(2*25.4+2)/1000
+y=np.arange(-7.5*a,8.5*a,a)
+
 for row in centers:
-    pixel = row[3:]
-    print(pixel)
+    x = row[3:]
+    A = np.vstack([x, np.ones(len(x))]).T
+    m, c = np.linalg.lstsq(A, y)[0]
+    print(m, c,m*255)
