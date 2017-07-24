d55db9dc405bcf80041a00d912ab64a9f4117db4
Ross Whitfield <whitfieldre@ornl.gov>
2017-07-24 12:19:05 -0400

    cron update

--- tube_calibration/create_ws.py
+++ tube_calibration/create_ws.py
@@ -1,2 +1,16 @@
+from mantid.simpleapi import Load, Integration, LoadEmptyInstrument
 import numpy as np
-ws_list=np.genfromtxt('list',usecols=(0,2))
+
+ws_list=np.genfromtxt('list',delimiter=',',dtype=int)
+
+accum = LoadEmptyInstrument(InstrumentName='CORELLI')
+
+for run, *banks in ws_list:
+    banks=np.asarray(banks)
+    banks = banks[np.nonzero(banks)]
+    banks=','.join('bank'+str(b) for b in banks)
+    print(run)
+    print(banks)
+    data = Load('CORELLI_'+str(run), BankName=banks)
+    data = Integration(data)
+    accum =+ data
