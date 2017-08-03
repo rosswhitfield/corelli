ece28d39edd5c69c37dc01c646af6fc1a2924720
Ross Whitfield <whitfieldre@ornl.gov>
2017-08-03 15:19:07 -0400

    cron update

--- FindUB/benzil_test3.py
+++ FindUB/benzil_test3.py
@@ -9,9 +9,9 @@ for r in runs:
     MaskBTP(data,Pixel='1-16,241-256')
     LoadInstrument(data, Filename='/SNS/CORELLI/shared/Calibration/CORELLI_Definition_cal_20160310.xml',RewriteSpectraMap=False)
     SetGoniometer(data,Axis0="BL9:Mot:Sample:Axis1,0,1,0,1")
-    ConvertToMD(InputWorkspace=data,OutputWorkspace='md2',QDimensions='Q3D',dEAnalysisMode='Elastic',
+    ConvertToMD(InputWorkspace=data,OutputWorkspace='md3',QDimensions='Q3D',dEAnalysisMode='Elastic',
                 QConversionScales='Q in A^-1',LorentzCorrection='0', MinValues='-15.1,-25.1,-15.1',MaxValues='15.1,25.1,15.1')
-    FindPeaksMD(InputWorkspace='md2',DensityThresholdFactor=50000, OutputWorkspace='peaks3',AppendPeaks=True)
+    FindPeaksMD(InputWorkspace='md3',DensityThresholdFactor=50000, OutputWorkspace='peaks3',AppendPeaks=True)
 FindUBUsingFFT(PeaksWorkspace='peaks3', MinD=5, MaxD=15)
 ShowPossibleCells(PeaksWorkspace='peaks3')
 SelectCellOfType(PeaksWorkspace='peaks3',CellType='Hexagonal',Apply=True)
