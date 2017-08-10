2103043852a09927d0ca8cca07b607a28cdbd2fd
Ross Whitfield <whitfieldre@ornl.gov>
2017-08-10 12:19:08 -0400

    cron update

--- FindUB/ConvertMutlipleRunsToSingleCrystalMD_test.py
+++ FindUB/ConvertMutlipleRunsToSingleCrystalMD_test.py
@@ -1,16 +1,15 @@
-UB = '/SNS/users/rwp/corelli/FindUB/benzil2.mat'
-
-ConvertMutlipleRunsToSingleCrystalMD(Filename='CORELLI_29533:29535',
+ConvertMultipleRunsToSingleCrystalMD(Filename='CORELLI_29782:29817:10',
                                      OutputWorkspace='output',
                                      SetGoniometer=True,
                                      FilterByTofMin=1000,
                                      FilterByTofMax=17000,
                                      Axis0="BL9:Mot:Sample:Axis1,0,1,0,1")
 
-ConvertMutlipleRunsToSingleCrystalMD(Filename='CORELLI_29533:29535',
+ConvertMultipleRunsToSingleCrystalMD(Filename='CORELLI_29782:29817:10',
                                      OutputWorkspace='output_ub',
                                      SetGoniometer=True,
                                      Axis0="BL9:Mot:Sample:Axis1,0,1,0,1",
                                      FilterByTofMin=1000,
                                      FilterByTofMax=17000,
-                                     UBMatrix=UB)
+                                     UBMatrix='/SNS/CORELLI/IPTS-15526/shared/benzil_Hexagonal.mat')
+
