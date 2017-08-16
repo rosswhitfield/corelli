689e9129f98a809182c9694daf3618f7aad37e03
Ross Whitfield <whitfieldre@ornl.gov>
2017-08-16 18:19:09 -0400

    cron update

--- find_rotation_offest/md.py
+++ find_rotation_offest/md.py
@@ -27,21 +27,19 @@ x=np.linspace(-1,1,1000)
 X,Y=np.meshgrid(x,x)
 mask = X**2 + Y**2 >1
 
-mask[s1[:,:,0]==0] = True
-mask[s2[:,:,0]==0] = True
+s1_mask = s1 < 100
+
+mask[s1_mask[:,:,0]] = True
+#mask[s2[:,:,0]==0] = True
 
 
 
 s1[mask]=np.nan
-s2[mask]=np.nan
-bin1.setSignalArray(s1)
-bin2.setSignalArray(s2)
-np.nansum(s1*s2)
 
 start=ws1.getRun().getLogData('BL9:Mot:Sample:Axis2').value.mean()
 
-for angle in np.arange(start-1,start+1,0.1):
-    SetGoniometer(ws2,Axis0=str(angle)+',0,1,0,1')
+for angle in np.arange(-0.1,0.1,0.02):
+    SetGoniometer(ws2,Axis0=str(start+angle)+',0,1,0,1')
     md2=ConvertToMD(ws2, QDimensions='Q3D', dEAnalysisMode='Elastic', Q3DFrames='Q_sample', MinValues=[-10,-10,-10], MaxValues=[10,10,10])
     bin2=BinMD(md2, AlignedDim0='Q_sample_x,-10,10,1000', AlignedDim1='Q_sample_z,-10,10,1000', AlignedDim2='Q_sample_y,-10,10,1')
     bin2/=ws2.run().getProtonCharge()
