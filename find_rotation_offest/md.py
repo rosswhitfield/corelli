67ba2c03b9a4f1c85feb70551477fb96423d4de5
Ross Whitfield <whitfieldre@ornl.gov>
2017-08-17 09:19:08 -0400

    cron update

--- find_rotation_offest/md.py
+++ find_rotation_offest/md.py
@@ -7,8 +7,8 @@ run2='CORELLI_48513'
 ws1=Load(run1)
 ws2=Load(run2)
 
-SetGoniometer(ws1,Axis0="BL9:Mot:Sample:Axis2,0,1,0,1")
-SetGoniometer(ws2,Axis0="BL9:Mot:Sample:Axis2,0,1,0,1")
+SetGoniometer(ws1,Axis0="BL9:Mot:Sample:Axis2.RBV,0,1,0,1")
+SetGoniometer(ws2,Axis0="BL9:Mot:Sample:Axis2.RBV,0,1,0,1")
 
 md1=ConvertToMD(ws1, QDimensions='Q3D', dEAnalysisMode='Elastic', Q3DFrames='Q_sample', MinValues=[-10,-10,-10], MaxValues=[10,10,10])
 md2=ConvertToMD(ws2, QDimensions='Q3D', dEAnalysisMode='Elastic', Q3DFrames='Q_sample', MinValues=[-10,-10,-10], MaxValues=[10,10,10])
@@ -36,9 +36,9 @@ mask[s1_mask[:,:,0]] = True
 
 s1[mask]=np.nan
 
-start=ws1.getRun().getLogData('BL9:Mot:Sample:Axis2').value.mean()
+start=ws1.getRun().getLogData('BL9:Mot:Sample:Axis2.RBV').value.mean()
 
-for angle in np.arange(-0.1,0.1,0.02):
+for angle in np.arange(-0.03,-0.01,0.001):
     SetGoniometer(ws2,Axis0=str(start+angle)+',0,1,0,1')
     md2=ConvertToMD(ws2, QDimensions='Q3D', dEAnalysisMode='Elastic', Q3DFrames='Q_sample', MinValues=[-10,-10,-10], MaxValues=[10,10,10])
     bin2=BinMD(md2, AlignedDim0='Q_sample_x,-10,10,1000', AlignedDim1='Q_sample_z,-10,10,1000', AlignedDim2='Q_sample_y,-10,10,1')
