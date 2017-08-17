32dbf4da4fc454474d1dcd011480e7919a2502e8
Ross Whitfield <whitfieldre@ornl.gov>
2017-08-17 18:19:06 -0400

    cron update

--- find_rotation_offest/md2.py
+++ find_rotation_offest/md2.py
@@ -38,7 +38,7 @@ start=ws2.getRun().getLogData('BL9:Mot:Sample:Axis2.RBV').value.mean()
 max_corr=0
 max_angle=0
 
-for angle in np.arange(-0.1,0.1,0.01):
+for angle in np.arange(-0.5,0.51,0.01):
     SetGoniometer(ws2,Axis0=str(start+angle)+',0,1,0,1')
     md2=ConvertToMD(ws2, QDimensions='Q3D', dEAnalysisMode='Elastic', Q3DFrames='Q_sample', MinValues=[-10,-10,-10], MaxValues=[10,10,10])
     bin2=BinMD(md2, AlignedDim0='Q_sample_x,-10,10,1000', AlignedDim1='Q_sample_z,-10,10,1000', AlignedDim2='Q_sample_y,-10,10,1')
