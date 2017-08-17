6b723dc092d7b4abc222b2489e9c5da15525df68
Ross Whitfield <whitfieldre@ornl.gov>
2017-08-17 12:19:07 -0400

    cron update

--- find_rotation_offest/md2.py
+++ find_rotation_offest/md2.py
@@ -1,8 +1,11 @@
-from mantid.simpleapi import Load, SetGoniometer, ConvertToMD
+from __future__ import print_function
+from mantid.simpleapi import Load, SetGoniometer, ConvertToMD, BinMD
 import numpy as np
+import sys
 
-run1='CORELLI_48505'
-run2='CORELLI_48513'
+run1='CORELLI_'+sys.argv[1]
+run2='CORELLI_'+sys.argv[2]
+filename=sys.argv[3]
 
 ws1=Load(run1)
 ws2=Load(run2)
@@ -11,17 +14,11 @@ SetGoniometer(ws1,Axis0="BL9:Mot:Sample:Axis2,0,1,0,1")
 SetGoniometer(ws2,Axis0="BL9:Mot:Sample:Axis2,0,1,0,1")
 
 md1=ConvertToMD(ws1, QDimensions='Q3D', dEAnalysisMode='Elastic', Q3DFrames='Q_sample', MinValues=[-10,-10,-10], MaxValues=[10,10,10])
-md2=ConvertToMD(ws2, QDimensions='Q3D', dEAnalysisMode='Elastic', Q3DFrames='Q_sample', MinValues=[-10,-10,-10], MaxValues=[10,10,10])
 
 bin1=BinMD(md1, AlignedDim0='Q_sample_x,-10,10,1000', AlignedDim1='Q_sample_z,-10,10,1000', AlignedDim2='Q_sample_y,-10,10,1')
-bin2=BinMD(md2, AlignedDim0='Q_sample_x,-10,10,1000', AlignedDim1='Q_sample_z,-10,10,1000', AlignedDim2='Q_sample_y,-10,10,1')
 bin1/=ws1.run().getProtonCharge()
-bin2/=ws2.run().getProtonCharge()
-diff0=bin2-bin1
-
 
 s1=bin1.getSignalArray().copy()
-#s2=bin2.getSignalArray().copy()
 
 x=np.linspace(-1,1,1000)
 X,Y=np.meshgrid(x,x)
@@ -30,12 +27,10 @@ mask = X**2 + Y**2 >1
 s1_mask = s1 < np.percentile(s1,99.995)
 
 mask[s1_mask[:,:,0]] = True
-#mask[s2[:,:,0]==0] = True
-
 
 s1[mask]=np.nan
 
-start=ws2.getRun().getLogData('BL9:Mot:Sample:Axis2.RBV').value.mean()
+start=ws1.getRun().getLogData('BL9:Mot:Sample:Axis2.RBV').value.mean()
 
 max_corr=0
 max_angle=0
@@ -52,4 +47,6 @@ for angle in np.arange(-0.02,0.02,0.001):
         max_corr = corr
         max_angle=angle
 
-print(max_angle)
+print("Actual = ",start+max_angle, "Diff = ", max_angle)
+with open(filename,'a') as f:
+    f.write(run1+' '+run2+' '+'Actual = '+str(start+max_angle)+'Diff = '+str(max_angle)
