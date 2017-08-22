461dc03929d7041e46148ec79bec242063fca858
Ross Whitfield <whitfieldre@ornl.gov>
2017-08-22 14:19:05 -0400

    cron update

--- MDNormSCDPreprocessIncoherent_unittest.py
+++ MDNormSCDPreprocessIncoherent_unittest.py
@@ -5,6 +5,7 @@ ws=Rebin(ws,'1,1.5,1.5')
 ws=CropWorkspace(ws,1.0,1.5)
 
 SA=Rebin(ws,'1,1.5,1.5',PreserveEvents=False)
+RemoveWorkspaceHistory(SA)
 
 ws=SumSpectra(ws)
 ws=Rebin(ws,'1,1.5,1.5')
@@ -12,6 +13,7 @@ el=ws.getSpectrum(0)
 el.divide(ws.readY(0)[0],ws.readE(0)[0])
 
 flux=IntegrateFlux(ws, NPoints=10000)
+RemoveWorkspaceHistory(flux)
 
 SaveNexus(flux,'flux,nxs')
-SaveNexus(SA,'sa,nxs')
+SaveNexus(SA,'sa.nxs')
