e479f68b1318a13cfc738d5a3c654dba46668d11
Ross Whitfield <whitfieldre@ornl.gov>
2017-10-10 17:20:26 -0400

    cron update

--- tube_calibration/testCalib2.py
+++ tube_calibration/testCalib2.py
@@ -1,5 +1,5 @@
 import tube
-tube.readCalibrationFile('CalibTable','/SNS/users/rwp/corelli/tube_calibration/out2')
+tube.readCalibrationFile('CalibTable','/SNS/users/rwp/corelli/tube_calibration/CalibTableNew.txt')
 
 data=LoadNexus('/SNS/users/rwp/corelli/tube_calibration/all_banks.nxs')
 org=CloneWorkspace(data)
