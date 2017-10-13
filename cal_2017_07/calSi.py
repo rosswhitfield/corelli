5ae2c870b2156f3653bd0771a852c2dbe8c40167
Ross Whitfield <whitfieldre@ornl.gov>
2017-10-13 10:19:04 -0400

    cron update

--- cal_2017_07/calSi.py
+++ cal_2017_07/calSi.py
@@ -1,9 +1,9 @@
 files='CORELLI_47327' # CORELLI_47327:47334
 
-wkpsName='out'
+wkspName='out'
 
-LoadEventAndCompress(Filename=files, OutputWorkspace=wkspName,
-                                          MaxChunkSize=16, FilterBadPulses=25)
+Load(Filename=files, OutputWorkspace=wkspName)
+
 CropWorkspace(InputWorkspace=wkspName, OutputWorkspace=wkspName, XMin=300, XMax=16666.7)
 
 dvalues = (1.1085, 1.2458, 1.3576, 1.6374, 1.9200, 3.1353)
@@ -13,4 +13,5 @@ PDCalibration(SignalWorkspace=wkspName,
               PeakPositions=dvalues,
               StartFromObservedPeakCentre=True,
               OutputCalibrationTable='new_cal',
-              CalibrationParameters='DIFC')
+              CalibrationParameters='DIFC',
+              DiagnosticWorkspaces='diag')
