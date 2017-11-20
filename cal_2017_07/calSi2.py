06a6ef9c4bdb20f3b4ed1df7cddb3dba06c7eb4a
Ross Whitfield <whitfieldre@ornl.gov>
2017-11-20 16:22:45 -0500

    cron update

--- cal_2017_07/calSi2.py
+++ cal_2017_07/calSi2.py
@@ -15,6 +15,36 @@ ConvertUnits(InputWorkspace='rawSi',OutputWorkspace='siliconD',Target='dSpacing'
 SumNeighbours(InputWorkspace="siliconD", OutputWorkspace="siliconD", SumX=1, SumY=16)
 Rebin(InputWorkspace='siliconD',OutputWorkspace='siliconD',Params='0.5,-0.004,3.5')
 
+GetDetOffsetsMultiPeaks(
+    InputWorkspace = 'siliconD',
+    DReference = FinalDReference,
+    FitWindowMaxWidth=0.1,
+    BackgroundType = "Flat",
+    OutputWorkspace = 'offset',
+    MaskWorkspace='mask')
+
+
+SaveCalFile(Filename='cal_Si2_47327-47334_sum16.cal',
+            OffsetsWorkspace="offset",
+            MaskWorkspace='mask')
+maskNumberPeaksFitted = np.where(mtd['NumberPeaksFitted'].extractY() <3)
+MaskDetectors('mask',DetectorList=maskNumberPeaksFitted[0])
+SaveCalFile(Filename='cal_Si2_47327-47334_sum16_mask_lt_3.cal',
+            OffsetsWorkspace="offset",
+            MaskWorkspace='mask')
+
+# With tube cal
+
+Load(Filename='CORELLI_47327-47334', OutputWorkspace='rawSi')
+ApplyCalibration('rawSi','CalibTable')
+SetInstrumentParameter(Workspace="rawSi",ParameterName="t0_formula",Value="(23.5 * exp(-incidentEnergy/205.8))")
+ModeratorTzero(InputWorkspace="rawSi",OutputWorkspace="rawSi",EMode="Elastic")
+    
+MaskBTP(Workspace='rawSi',Pixel="1-16,241-256")
+ConvertUnits(InputWorkspace='rawSi',OutputWorkspace='siliconD',Target='dSpacing')
+SumNeighbours(InputWorkspace="siliconD", OutputWorkspace="siliconD", SumX=1, SumY=16)
+Rebin(InputWorkspace='siliconD',OutputWorkspace='siliconD',Params='0.5,-0.004,3.5')
+
 
 GetDetOffsetsMultiPeaks(
     InputWorkspace = 'siliconD',
@@ -33,3 +63,7 @@ MaskDetectors('mask',DetectorList=maskNumberPeaksFitted[0])
 SaveCalFile(Filename='cal_Si2_47327-47334_sum16_mask_lt_3.cal',
             OffsetsWorkspace="offset",
             MaskWorkspace='mask')
+
+
+
+
