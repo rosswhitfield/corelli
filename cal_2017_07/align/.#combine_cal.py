ef3d6880c5835dbf23e029c9db858e7c5577f95d
Ross Whitfield <whitfieldre@ornl.gov>
2017-11-27 15:20:41 -0500

    cron update

---
+++ cal_2017_07/align/.#combine_cal.py
@@ -0,0 +1,1 @@
+rwp@diffuse.ornl.gov.28482:1511795943
--- cal_2017_07/align/combine_cal.py
+++ cal_2017_07/align/combine_cal.py
@@ -1,4 +1,5 @@
 from mantid.simpleapi import LoadDiffCal, mtd
+import matplotlib.pyplot as plt
 
 LoadDiffCal(Filename='../cal_Si_C60/cal_Si2_47327-47334_TubeCal_sum16_mask_lt_3.cal',
             InstrumentName='CORELLI',
@@ -7,3 +8,4 @@ LoadDiffCal(Filename='../cal_Si_C60/cal_Si2_47327-47334_TubeCal_sum16_mask_lt_3.
 LoadDiffCal(Filename='../cal_Si_C60/cal_C60_2_47367-47382_TubeCal_sum16_mask_lt_3.h5',
             InstrumentName='CORELLI',
             WorkspaceName='c60')
+
