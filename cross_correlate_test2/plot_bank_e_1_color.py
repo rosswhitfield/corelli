0e0426783bfdde2ba4d5937ed4ebdb985e49e80f
Ross Whitfield <whitfieldre@ornl.gov>
2018-01-08 14:19:05 -0500

    cron update

--- cross_correlate_test2/plot_bank_e_1_color.py
+++ cross_correlate_test2/plot_bank_e_1_color.py
@@ -2,6 +2,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+colour = 'Greys'
+
 fig = plt.figure(figsize=(12,6))
 total = np.zeros((341,16667))
 for bank in range(31,62):
@@ -16,20 +18,19 @@ for bank in range(31,62):
     yy=np.array(range(y))*10
     X,Y = np.meshgrid(xx,yy)
     plt.clf()
-    plt.pcolormesh(X, Y, results, vmin=0, vmax=100,cmap='viridis')
+    plt.pcolormesh(X, Y, results, vmin=0, vmax=200,cmap=colour)
     #plt.pcolormesh(X, Y, results)
     plt.colorbar()
     plt.title('bank'+str(bank))
     plt.xlabel('total time of flight (uS)')
     plt.ylabel('incident flight time (uS)')
-    plt.savefig('results_events_bank'+str(bank)+'_viridis.png')
+    plt.savefig('results_events_bank'+str(bank)+'_'+colour+'.png')
 
 plt.clf()
-plt.pcolormesh(X, Y, total, vmin=200, vmax=1000, cmap='Greys')
+plt.pcolormesh(X, Y, total, vmin=00, vmax=2000, cmap=colour)
 #plt.pcolormesh(X, Y, total)
 plt.colorbar()
 plt.title('all banks')
 plt.xlabel('total time of flight (uS)')
 plt.ylabel('incident flight time (uS)')
-plt.savefig('results_events_all_bank_viridis.png')
-
+plt.savefig('results_events_all_bank_'+colour+'.png')
