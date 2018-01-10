c47cbd70d4023aca73ea9653e09a3b76d6ec8851
Ross Whitfield <whitfieldre@ornl.gov>
2018-01-10 12:19:04 -0500

    cron update

--- cross_correlate_test2/export_bank_e_1_r_errors_rebin2.py
+++ cross_correlate_test2/export_bank_e_1_r_errors_rebin2.py
@@ -4,17 +4,19 @@ import numpy as np
 total = np.zeros((341,16667))
 error = np.zeros(16667)
 for bank in range(31,62):
-    results = np.zeros((341,16667))
     for run in range(30338,30346):
         filename = 'CORELLI_'+str(run)+'_results_events_b1_bank_r2.584_'+str(bank)+'.npy'
         temp=np.load(filename)
-        results += temp
         total += temp
         filename = 'CORELLI_'+str(run)+'_results_events_b1_bank_r2.584_errorSq_'+str(bank)+'.npy'
         temp=np.load(filename)
         error += temp
 
-y,x = results.shape
+
+total = total[:,:15120]
+error = error[:15120]
+
+y,x = total.shape
 xx=np.array(range(x))*1
         
 
@@ -26,6 +28,11 @@ m=1.674927351e-27
 
 # 1D define Ei
 
+for bin_size in range(1,11):
+    error_tmp = error.reshape(-1, bin_size).sum(axis=1)
+    total_tmp = total.reshape((341, -1, bin_size)).sum(axis=2)
+    xx_tmp = xx.reshape(-1, bin_size).average(axis=1)
+
 for mev, offset, x_offset in [(12,45,0.05), (15,45,-0.03), (20,45,0.18), (25,40,0.3), (30,40,0.26), (50,50,-0.03), (75,50,-0.3)]:
     print(mev,offset)    
     ei = mev/1e3 * 1.602e-19
@@ -35,24 +42,12 @@ for mev, offset, x_offset in [(12,45,0.05), (15,45,-0.03), (20,45,0.18), (25,40,
     tof = (L1+L2)/vi
     xi = int(tof*1e6)
     t1 = L1/vi
-    ix = range(xi-1000,xi+1000)
+    ix = range(xi-200,xi+200)
     t2 = xx[ix]/1e6 - t1
     v2 = L2/t2
     ef = 0.5*m*v2**2
     dEs = (ei-ef)/1.602e-19*1000 - x_offset
-    ints = total[int((y-offset)/10),ix]
-    e = np.sqrt(error[ix])
+    ints = total_tmp[int((y-offset)/10),ix]
+    e = np.sqrt(error_tmp[ix])
     mask = np.abs(dEs) < 10
-    np.savetxt("Ei_"+str(mev)+"_meV.xye",np.array([dEs[mask],ints[mask],e[mask]]).T)
-    dEs = (dEs[::2]+dEs[1::2])/2
-    ints = ints[::2]+ints[1::2]
-    e_tmp = error[ix]
-    e = np.sqrt(e_tmp[::2]+e_tmp[1::2])
-    mask = np.abs(dEs) < 10
-    np.savetxt("Ei_"+str(mev)+"_meV_x2.xye",np.array([dEs[mask],ints[mask],e[mask]]).T)
-    dEs = (dEs[::2]+dEs[1::2])/2
-    ints = ints[::2]+ints[1::2]
-    e_tmp = error[ix]
-    e = np.sqrt(e_tmp[::4]+e_tmp[1::4]+e_tmp[2::4]+e_tmp[3::4])
-    mask = np.abs(dEs) < 10
-    np.savetxt("Ei_"+str(mev)+"_meV_x4.xye",np.array([dEs[mask],ints[mask],e[mask]]).T)
+    np.savetxt("Ei_"+str(mev)+"_meV_"+str(bin_size)+"us.xye",np.array([dEs[mask],ints[mask],e[mask]]).T)
