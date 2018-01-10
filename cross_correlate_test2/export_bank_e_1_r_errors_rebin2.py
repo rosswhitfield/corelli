165246f2e28a051685060a86352ccaf76b36557e
Ross Whitfield <whitfieldre@ornl.gov>
2018-01-10 13:19:04 -0500

    cron update

--- cross_correlate_test2/export_bank_e_1_r_errors_rebin2.py
+++ cross_correlate_test2/export_bank_e_1_r_errors_rebin2.py
@@ -28,26 +28,26 @@ m=1.674927351e-27
 
 # 1D define Ei
 
-for bin_size in range(1,11):
+for bin_size in range(1,21):
+    if bin_size in (11, 13, 17, 19):
+        continue
     error_tmp = error.reshape(-1, bin_size).sum(axis=1)
     total_tmp = total.reshape((341, -1, bin_size)).sum(axis=2)
-    xx_tmp = xx.reshape(-1, bin_size).average(axis=1)
-
-for mev, offset, x_offset in [(12,45,0.05), (15,45,-0.03), (20,45,0.18), (25,40,0.3), (30,40,0.26), (50,50,-0.03), (75,50,-0.3)]:
-    print(mev,offset)    
-    ei = mev/1e3 * 1.602e-19
-    vi = np.sqrt(2*ei/m)
-    ti = Lmc/vi
-    y = ti*1e6%chopper_per
-    tof = (L1+L2)/vi
-    xi = int(tof*1e6)
-    t1 = L1/vi
-    ix = range(xi-200,xi+200)
-    t2 = xx[ix]/1e6 - t1
-    v2 = L2/t2
-    ef = 0.5*m*v2**2
-    dEs = (ei-ef)/1.602e-19*1000 - x_offset
-    ints = total_tmp[int((y-offset)/10),ix]
-    e = np.sqrt(error_tmp[ix])
-    mask = np.abs(dEs) < 10
-    np.savetxt("Ei_"+str(mev)+"_meV_"+str(bin_size)+"us.xye",np.array([dEs[mask],ints[mask],e[mask]]).T)
+    xx_tmp = np.average(xx.reshape(-1, bin_size), axis=1)
+    for mev, offset, x_offset in [(12,45,0.05), (15,45,-0.03), (20,45,0.18), (25,40,0.3), (30,40,0.26), (50,50,-0.03), (75,50,-0.3)]:
+        print(mev,offset)    
+        ei = mev/1e3 * 1.602e-19
+        vi = np.sqrt(2*ei/m)
+        ti = Lmc/vi
+        y = ti*1e6%chopper_per
+        tof = (L1+L2)/vi
+        xi = int(tof*1e6/bin_size)
+        t1 = L1/vi
+        t2 = xx_tmp/1e6 - t1
+        v2 = L2/t2
+        ef = 0.5*m*v2**2
+        dEs = (ei-ef)/1.602e-19*1000 - x_offset
+        ints = total_tmp[int((y-offset)/10),:]
+        e = np.sqrt(error_tmp)
+        mask = np.logical_and(np.abs(dEs) < 10, xx_tmp - xi < 1000)
+        np.savetxt("Ei_"+str(mev)+"_meV_"+str(bin_size)+"us.xye",np.array([dEs[mask],ints[mask],e[mask]]).T)
