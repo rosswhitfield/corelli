aba5a6691d6df81bddbb37f0496528cf5fc0e6a1
Ross Whitfield <whitfieldre@ornl.gov>
2018-06-06 11:19:07 -0400

    Update cal_2018_05/single_bank/cal_C60_58_4.py

--- cal_2018_05/single_bank/cal_C60_58_4.py
+++ cal_2018_05/single_bank/cal_C60_58_4.py
@@ -38,12 +38,13 @@ PDCalibration(InputWorkspace='rawC60_binned',
 
 PDCalibration(InputWorkspace='rawC60_binned',
               TofBinning=TofBinning,
+              PreviousCalibrationTable='cal2B',
               PeakPositions=DReference,
               MinimumPeakHeight=5,
               PeakWidthPercent=0.01,
-              PeakFunction='Lorentzian',
-              OutputCalibrationTable='cal2BL',
-              DiagnosticWorkspaces='diag2BL')
+              PeakWindow=0.5,
+              OutputCalibrationTable='cal2B_s2',
+              DiagnosticWorkspaces='diag2B_s2')
 
 
 # Compare in d
@@ -58,8 +59,8 @@ AlignDetectors('rawC60', OutputWorkspace='rawC60_d_2', CalibrationWorkspace='cal
 DiffractionFocussing('rawC60_d_2', OutputWorkspace='rawC60_d_2', GroupingWorkspace='group')
 AlignDetectors('rawC60', OutputWorkspace='rawC60_d_2B', CalibrationWorkspace='cal2B')
 DiffractionFocussing('rawC60_d_2B', OutputWorkspace='rawC60_d_2B', GroupingWorkspace='group')
-AlignDetectors('rawC60', OutputWorkspace='rawC60_d_2BL', CalibrationWorkspace='cal2BL')
-DiffractionFocussing('rawC60_d_2BL', OutputWorkspace='rawC60_d_2BL', GroupingWorkspace='group')
+AlignDetectors('rawC60', OutputWorkspace='rawC60_d_2B_s2', CalibrationWorkspace='cal2B_s2')
+DiffractionFocussing('rawC60_d_2B_s2', OutputWorkspace='rawC60_d_2B_s2', GroupingWorkspace='group')
 
 
 import matplotlib.pyplot as plt
@@ -68,7 +69,7 @@ fig, ax = plt.subplots(subplot_kw={'projection':'mantid'})
 ax.plot(mtd['rawC60d'],label='rawC60d')
 ax.plot(mtd['rawC60_d_2'],label='rawC60_d_2')
 ax.plot(mtd['rawC60_d_2B'],label='rawC60_d_2B')
-ax.plot(mtd['rawC60_d_2BL'],label='rawC60_d_2BL')
+ax.plot(mtd['rawC60_d_2B_s2'],label='rawC60_d_2B_s2')
 for d in DReference:
     ax.axvline(d,linewidth=1)
 ax.legend()
