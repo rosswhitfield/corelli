5601f0aba88ecf018deba3441169931730458c1e
Ross Whitfield <whitfieldre@ornl.gov>
2018-06-06 18:19:07 -0400

    Update cal_2018_05/0/compare.py cal_2018_05/0a/.#AlignC60_all_y.py cal_2018_05/0a/AlignC60_all_y.py cal_2018_05/0a/AlignSi.py

---
+++ cal_2018_05/0/compare.py
@@ -0,0 +1,22 @@
+import numpy as np
+from mantid.simpleapi import *
+import tube
+tube.readCalibrationFile('CalibTable','/SNS/users/rwp/corelli/tube_calibration2/CalibTable2_combined.txt')
+
+corelli=LoadEmptyInstrument(InstrumentName='CORELLI')
+ApplyCalibration('corelli','CalibTable')
+LoadDiffCal(Filename='/SNS/users/rwp/corelli/cal_2018_05/0/calB_si.h5',InstrumentName='CORELLI',WorkspaceName='si')
+
+difc=CalculateDIFC('corelli')
+difc_si=CalculateDIFC('corelli',CalibrationWorkspace='si_cal')
+
+diff_si = difc/difc_si
+
+
+LoadDiffCal(Filename='/SNS/users/rwp/corelli/cal_2018_05/0/cal_c60.h5',InstrumentName='CORELLI',WorkspaceName='c60')
+
+difc_c60=CalculateDIFC('corelli',CalibrationWorkspace='c60_cal')
+
+diff_c60 = difc/difc_c60
+
+
