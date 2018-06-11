3543f5b9aa7a8bdea8801f6be02d1ae122a93382
Ross Whitfield <whitfieldre@ornl.gov>
2018-06-11 14:19:09 -0400

    Update cal_2018_05/0a/.#export.py cal_2018_05/0a/export.py

--- cal_2018_05/0a/export.py
+++ cal_2018_05/0a/export.py
@@ -1,6 +1,10 @@
+from mantid.simpleapi import *
 import tube
 tube.readCalibrationFile('CalibTable','/SNS/users/rwp/corelli/tube_calibration2/CalibTable2_combined.txt')
 
+SaveNexus('CalibTable','CalibTable.nxs')
+
 corelli=LoadEmptyInstrument(InstrumentName='CORELLI')
 ApplyCalibration('corelli','CalibTable')
-LoadIsawDetCal('rawC60','/SNS/users/rwp/corelli/cal_2018_05/0a/Aligned_row_si_c60.nxs.detcal')
+LoadIsawDetCal('corelli','/SNS/users/rwp/corelli/cal_2018_05/0a/Aligned_row_si_c60.nxs.detcal')
+SaveNexus('corelli','TubeCalib+DetCal.nxs')
