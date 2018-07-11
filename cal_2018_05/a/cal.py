5868e4db01d26c80075ec628446485bf18e74b63
Ross Whitfield <whitfieldre@ornl.gov>
2018-07-11 13:19:12 -0400

    Update cal_2018_05/a/.#cal.py cal_2018_05/a/cal.py

--- cal_2018_05/a/cal.py
+++ cal_2018_05/a/cal.py
@@ -67,3 +67,13 @@ c60_diff = c60_difc/difc
 
 combined_cal = CloneWorkspace('Si_cal')
 combined_mask = CloneWorkspace('Si_cal_mask')
+
+for bank in [22,23,24,25,26,27,28, 53,54,55,56,57,58,59,60,61, 84,85,86,87,88,89,90]:
+    for detID in range((bank-1)*4096,bank*4096):
+        combined_mask.setY(detID, mtd['C60_cal_mask'].readY(detID))
+        combined_cal.setCell(detID, 1, mtd['C60_cal'].cell(detID, 1))
+
+combined_difc = CalculateDIFC('rawSi', CalibrationWorkspace='combined_cal')
+combined_diff = combined_difc/difc
+
+SaveDiffCal(CalibrationWorkspace='combined_cal', MaskWorkspace='combined_mask', Filename='/SNS/users/rwp/corelli/cal_2018_05/a/cal.h5')
