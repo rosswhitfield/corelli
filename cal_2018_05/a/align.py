ff92816a83ef9385af2ecc355a7ab43c8b988021
Ross Whitfield <whitfieldre@ornl.gov>
2018-07-13 16:19:10 -0400

    Update cal_2018_05/a/align.py

--- cal_2018_05/a/align.py
+++ cal_2018_05/a/align.py
@@ -5,27 +5,22 @@ tube.readCalibrationFile('CalibTable','/SNS/users/rwp/corelli/tube_calibration2/
 
 corelli=LoadEmptyInstrument(InstrumentName='CORELLI')
 ApplyCalibration('corelli','CalibTable')
+LoadIsawDetCal('corelli','/SNS/users/rwp/corelli/cal_2018_05/a/Aligned.nxs.detcal')
+
 difc_old=CalculateDIFC('corelli')
 
 LoadDiffCal(Filename='/SNS/users/rwp/corelli/cal_2018_05/a/cal.h5',InstrumentName='CORELLI',WorkspaceName='cal')
 
-MaskBTP('mask',Bank='26',Tube='1-8')
-MaskBTP('mask',Pixel='1-16,241-256')
+MaskBTP('cal_mask',Bank='26',Tube='1-8')
+MaskBTP('cal_mask',Pixel='1-16,241-256')
 
-# Kick them so they start moving
-RotateInstrumentComponent('corelli', ComponentName='A row',Y=1,angle=0.01)
-RotateInstrumentComponent('corelli', ComponentName='B row',Y=1,angle=0.01)
-RotateInstrumentComponent('corelli', ComponentName='C row',Y=1,angle=0.01)
 
-AlignComponents(Workspace='corelli',CalibrationTable="cal_cal",MaskWorkspace="cal_mask",ComponentList="A row,B row,C row",Xposition=True,Yposition=True,Zposition=True,AlphaRotation=True)
-print(mtd['corelli'].getInstrument().getComponentByName('A row').getPos())
-print(mtd['corelli'].getInstrument().getComponentByName('B row').getPos())
-print(mtd['corelli'].getInstrument().getComponentByName('C row').getPos())
-print(mtd['corelli'].getInstrument().getComponentByName('A row').getRotation().getEulerAngles())
-print(mtd['corelli'].getInstrument().getComponentByName('B row').getRotation().getEulerAngles())
-print(mtd['corelli'].getInstrument().getComponentByName('C row').getRotation().getEulerAngles())
+componentList =  ','.join("bank"+str(i)+"/sixteenpack" for i in range(1,92))
 
-SaveNexus('corelli','/SNS/users/rwp/corelli/cal_2018_05/a/Aligned.nxs')
+#AlignComponents(Workspace='corelli',CalibrationTable="cal_cal",MaskWorkspace="cal_mask",ComponentList=componentList,GammaRotation=True)
+AlignComponents(Workspace='corelli',CalibrationTable="cal_cal",MaskWorkspace="cal_mask",ComponentList=componentList,GammaRotation=True,Xposition=True,Zposition=True)
+
+SaveNexus('corelli','/SNS/users/rwp/corelli/cal_2018_05/a/Aligned2.nxs')
 
 difc_new=CalculateDIFC('corelli')
 difc_diff = difc_new/difc_old
