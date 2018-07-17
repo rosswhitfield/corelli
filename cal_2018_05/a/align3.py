efc1cd21b88710ffd2325024b52acd10de93f248
Ross Whitfield <whitfieldre@ornl.gov>
2018-07-17 11:19:09 -0400

    Update cal_2018_05/a/align3.py cal_2018_05/a/cal4.py cal_2018_05/a/plot_cal.py

---
+++ cal_2018_05/a/align3.py
@@ -0,0 +1,35 @@
+import numpy as np
+from mantid.simpleapi import *
+import tube
+tube.readCalibrationFile('CalibTable','/SNS/users/rwp/corelli/tube_calibration2/CalibTable2_combined.txt')
+
+corelli=LoadEmptyInstrument(InstrumentName='CORELLI')
+ApplyCalibration('corelli','CalibTable')
+LoadIsawDetCal('corelli','/SNS/users/rwp/corelli/cal_2018_05/a/Aligned2.nxs.detcal')
+
+difc_old=CalculateDIFC('corelli')
+
+LoadDiffCal(Filename='/SNS/users/rwp/corelli/cal_2018_05/a/cal3.h5',InstrumentName='CORELLI',WorkspaceName='cal')
+
+MaskBTP('cal_mask',Bank='26',Tube='1-8')
+MaskBTP('cal_mask',Bank='74',Tube='13')
+MaskBTP('cal_mask',Pixel='1-16,241-256')
+
+
+# Kick them so they start moving
+RotateInstrumentComponent('corelli', ComponentName='A row',Y=1,angle=0.01)
+RotateInstrumentComponent('corelli', ComponentName='B row',Y=1,angle=0.01)
+RotateInstrumentComponent('corelli', ComponentName='C row',Y=1,angle=0.01)
+
+AlignComponents(Workspace='corelli',CalibrationTable="cal_cal",MaskWorkspace="cal_mask",ComponentList="A row,B row,C row",Xposition=True,Yposition=True,Zposition=True,AlphaRotation=True)
+print(mtd['corelli'].getInstrument().getComponentByName('A row').getPos())
+print(mtd['corelli'].getInstrument().getComponentByName('B row').getPos())
+print(mtd['corelli'].getInstrument().getComponentByName('C row').getPos())
+print(mtd['corelli'].getInstrument().getComponentByName('A row').getRotation().getEulerAngles())
+print(mtd['corelli'].getInstrument().getComponentByName('B row').getRotation().getEulerAngles())
+print(mtd['corelli'].getInstrument().getComponentByName('C row').getRotation().getEulerAngles())
+
+SaveNexus('corelli','/SNS/users/rwp/corelli/cal_2018_05/a/Aligned3.nxs')
+
+difc_new=CalculateDIFC('corelli')
+difc_diff = difc_new/difc_old
--- cal_2018_05/a/plot_cal.py
+++ cal_2018_05/a/plot_cal.py
@@ -13,10 +13,14 @@ with h5py.File('cal2.h5', 'r') as f:
 with h5py.File('cal3.h5', 'r') as f:
     cal3= f['calibration/difc'].value
 
+with h5py.File('cal4.h5', 'r') as f:
+    cal4= f['calibration/difc'].value
+
 
 plt.plot(cal1/cal_org,label='cal1')
 plt.plot(cal2/cal_org,label='cal2')
 plt.plot(cal3/cal_org,label='cal3')
+plt.plot(cal4/cal_org,label='cal4')
 plt.legend()
 plt.show()
 
@@ -25,5 +29,6 @@ plt.show()
 plt.plot(cal1/cal_org,label='cal1')
 plt.plot(cal2/cal1,label='cal2')
 plt.plot(cal3/cal2,label='cal3')
+plt.plot(cal4/cal3,label='cal4')
 plt.legend()
 plt.show()
