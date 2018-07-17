e53d7f71fe83e7161daa5212b02f03b5bd8c31d7
Ross Whitfield <whitfieldre@ornl.gov>
2018-07-17 12:19:09 -0400

    Update cal_2018_05/a/align4.py

---
+++ cal_2018_05/a/align4.py
@@ -0,0 +1,27 @@
+import numpy as np
+from mantid.simpleapi import *
+import tube
+tube.readCalibrationFile('CalibTable','/SNS/users/rwp/corelli/tube_calibration2/CalibTable2_combined.txt')
+
+corelli=LoadEmptyInstrument(InstrumentName='CORELLI')
+ApplyCalibration('corelli','CalibTable')
+LoadIsawDetCal('corelli','/SNS/users/rwp/corelli/cal_2018_05/a/Aligned3.nxs.detcal')
+
+difc_old=CalculateDIFC('corelli')
+
+LoadDiffCal(Filename='/SNS/users/rwp/corelli/cal_2018_05/a/cal4.h5',InstrumentName='CORELLI',WorkspaceName='cal')
+
+MaskBTP('cal_mask',Bank='26',Tube='1-8')
+MaskBTP('cal_mask',Bank='74',Tube='13')
+MaskBTP('cal_mask',Pixel='1-16,241-256')
+
+
+componentList =  ','.join("bank"+str(i)+"/sixteenpack" for i in range(1,92))
+
+#AlignComponents(Workspace='corelli',CalibrationTable="cal_cal",MaskWorkspace="cal_mask",ComponentList=componentList,GammaRotation=True)
+AlignComponents(Workspace='corelli',CalibrationTable="cal_cal",MaskWorkspace="cal_mask",ComponentList=componentList,GammaRotation=True,Xposition=True,YPosition=True,Zposition=True)
+
+SaveNexus('corelli','/SNS/users/rwp/corelli/cal_2018_05/a/Aligned4.nxs')
+
+difc_new=CalculateDIFC('corelli')
+difc_diff = difc_new/difc_old
