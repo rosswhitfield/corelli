de4cf593875307f10cd5d5e8f78e114a3337a430
Ross Whitfield <whitfieldre@ornl.gov>
2018-07-18 13:19:09 -0400

    Update cal_2018_05/a/extract_difc.py cal_2018_05/a/plot_cal.py

--- cal_2018_05/a/plot_cal.py
+++ cal_2018_05/a/plot_cal.py
@@ -6,6 +6,7 @@ cal_org = np.loadtxt('difc_org.txt')
 cal_aligned = np.loadtxt('difc_aligned.txt')
 cal_aligned2 = np.loadtxt('difc_aligned2.txt')
 cal_aligned3 = np.loadtxt('difc_aligned3.txt')
+cal_aligned4 = np.loadtxt('difc_aligned4.txt')
 
 with h5py.File('cal.h5', 'r') as f:
     cal1= f['calibration/difc'].value
@@ -19,11 +20,15 @@ with h5py.File('cal3.h5', 'r') as f:
 with h5py.File('cal4.h5', 'r') as f:
     cal4= f['calibration/difc'].value
 
+with h5py.File('cal5.h5', 'r') as f:
+    cal5= f['calibration/difc'].value
+
 
 plt.plot(cal1/cal_org,label='cal1')
 plt.plot(cal2/cal_aligned,label='cal2')
 plt.plot(cal3/cal_aligned2,label='cal3')
 plt.plot(cal4/cal_aligned3,label='cal4')
+plt.plot(cal5/cal_aligned4,label='cal5')
 plt.legend()
 plt.show()
 
@@ -32,5 +37,6 @@ plt.plot(cal1/cal_org,label='cal1')
 plt.plot(cal2/cal1,label='cal2')
 plt.plot(cal3/cal2,label='cal3')
 plt.plot(cal4/cal3,label='cal4')
+plt.plot(cal5/cal4,label='cal5')
 plt.legend()
 plt.show()
