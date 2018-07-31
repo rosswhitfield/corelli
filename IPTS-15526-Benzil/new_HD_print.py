cbbcc788429ff988a0380ec7fd3e32feda89a9ad
Ross Whitfield <whitfieldre@ornl.gov>
2018-07-31 13:19:06 -0400

    Update IPTS-15526-Benzil/.#new_HD_print.py IPTS-15526-Benzil/new_HD_print.py

--- IPTS-15526-Benzil/new_HD_print.py
+++ IPTS-15526-Benzil/new_HD_print.py
@@ -40,3 +40,37 @@ SingleCrystalDiffuseReduction(Filename='CORELLI_29782:29817',
                               BinningDim2='-0.1,1.1,6',
                               SymmetryOps="P 31 2 1")
 SaveMD('output', Filename='/SNS/users/rwp/benzil/benzil_300K_volume_1251_1251.nxs')
+
+out=mtd['output'].getSignalArray()
+
+import matplotlib
+matplotlib.image.imsave('benzil_300K_hk0.png', out[:,:,0], vmin=2.5e-6, vmax=1e-5)
+matplotlib.image.imsave('benzil_300K_hk1.png', out[:,:,5], vmin=2.5e-6, vmax=1e-5)
+
+
+
+
+# 100K 
+
+SingleCrystalDiffuseReduction(Filename='CORELLI_29715:29755,29589:29625,29533:29536,29556:29589',
+                              Background='CORELLI_28124',
+                              BackgroundScale=0.95,
+                              SolidAngle=sa,
+                              Flux=flux,
+                              UBMatrix="/SNS/CORELLI/IPTS-15526/shared/benzil_Hexagonal.mat",
+                              OutputWorkspace='output',
+                              SetGoniometer=True,
+                              Axis0="BL9:Mot:Sample:Axis1,0,1,0,1",
+                              Uproj='1,1,0',
+                              Vproj='1,-1,0',
+                              Wproj='0,0,1',
+                              BinningDim0='-6.255,6.255,1251',
+                              BinningDim1='-10.849991275878653,10.849991275878653,1251',
+                              BinningDim2='-0.1,1.1,6',
+                              SymmetryOps="P 31 2 1")
+SaveMD('output', Filename='/SNS/users/rwp/benzil/benzil_100K_volume_1251_1251.nxs')
+
+out=mtd['output'].getSignalArray()
+
+matplotlib.image.imsave('benzil_100K_hk0.png', out[:,:,0], vmin=2.5e-6, vmax=1e-5)
+matplotlib.image.imsave('benzil_100K_hk1.png', out[:,:,5], vmin=2.5e-6, vmax=1e-5)
