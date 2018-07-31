6a32a8df1adb51d9ecda02418a9c9c8e0046cbbe
Ross Whitfield <whitfieldre@ornl.gov>
2018-07-31 12:19:06 -0400

    Update IPTS-15526-Benzil/new_HD_print.py

--- IPTS-15526-Benzil/new_HD_print.py
+++ IPTS-15526-Benzil/new_HD_print.py
@@ -20,3 +20,23 @@ SingleCrystalDiffuseReduction(Filename='CORELLI_29782:29817',
                               BinningDim2='-0.1,1.1,6',
                               SymmetryOps="P 31 2 1")
 SaveMD('output', Filename='/SNS/users/rwp/benzil/benzil_300K_volume_801_801.nxs')
+
+
+
+SingleCrystalDiffuseReduction(Filename='CORELLI_29782:29817',
+                              Background='CORELLI_28124',
+                              BackgroundScale=0.95,
+                              SolidAngle=sa,
+                              Flux=flux,
+                              UBMatrix="/SNS/CORELLI/IPTS-15526/shared/benzil_Hexagonal.mat",
+                              OutputWorkspace='output',
+                              SetGoniometer=True,
+                              Axis0="BL9:Mot:Sample:Axis1,0,1,0,1",
+                              Uproj='1,1,0',
+                              Vproj='1,-1,0',
+                              Wproj='0,0,1',
+                              BinningDim0='-6.255,6.255,1251',
+                              BinningDim1='-10.849991275878653,10.849991275878653,1251',
+                              BinningDim2='-0.1,1.1,6',
+                              SymmetryOps="P 31 2 1")
+SaveMD('output', Filename='/SNS/users/rwp/benzil/benzil_300K_volume_1251_1251.nxs')
