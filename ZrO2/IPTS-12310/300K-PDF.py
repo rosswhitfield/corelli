2b17ce326d9d94b48f5f3d213269ee0031fb6e1b
Ross Whitfield <whitfieldre@ornl.gov>
2018-11-02 10:21:01 -0400

    Update ZrO2/IPTS-12310/300K-PDF.py ZrO2/IPTS-12310/van.py

--- ZrO2/IPTS-12310/300K-PDF.py
+++ ZrO2/IPTS-12310/300K-PDF.py
@@ -1,6 +1,6 @@
 outDir = '/SNS/users/rwp/corelli/ZrO2/IPTS-12310/'
 
-ConvertMultipleRunsToSingleCrystalMD(Filename='CORELLI_8568:8601',SetGoniometer=True,Axis0='BL9:Mot:Sample:Axis1,0,1,0,1',OutputWorkspace='md',MinValues=[-15,-15,-15],MaxValues=[15,15,15])
+ConvertMultipleRunsToSingleCrystalMD(Filename='CORELLI_8569:8601',SetGoniometer=True,Axis0='BL9:Mot:Sample:Axis1,0,1,0,1',OutputWorkspace='md',MinValues=[-15,-15,-15],MaxValues=[15,15,15])
 FindPeaksMD(InputWorkspace='md', PeakDistanceThreshold=0.25, DensityThresholdFactor=1000000, OutputWorkspace='peaks')
 FindUBUsingFFT(PeaksWorkspace='peaks', MinD=4, MaxD=6)
 ShowPossibleCells(PeaksWorkspace='peaks')
@@ -12,6 +12,7 @@ SaveIsawUB('peaks', outDir+'ZrO2_300K.mat')
 SingleCrystalDiffuseReduction(Filename='CORELLI_8192:8246',
                               SolidAngle=outDir+'SA.nxs',
                               Flux=outDir+'Flux.nxs',
+                              MaskFile=outDir+'mask.xml',
                               OutputWorkspace='output',
                               SetGoniometer=True,
                               Axis0="BL9:Mot:Sample:Axis1,0,1,0,1",
--- ZrO2/IPTS-12310/van.py
+++ ZrO2/IPTS-12310/van.py
@@ -5,8 +5,11 @@ LoadEmptyInstrument(InstrumentName='CORELLI', OutputWorkspace='CORELLI')
 MaskBTP('CORELLI', Bank='1-6,14-30,62-72,81-91')
 # End of tubes
 MaskBTP('CORELLI', Pixel='1-15,242-256')
-# Dead tube
+# Dead tubes
 MaskBTP('CORELLI', Bank='45', Tube='16')
+MaskBTP('CORELLI', Bank='49', Tube='1')
+MaskBTP('CORELLI', Bank='73', Tube='13')
+MaskBTP('CORELLI', Bank='80', Tube='6')
 # Beam
 MaskBTP('CORELLI', Bank='58', Tube='13-16', Pixel='65-145')
 MaskBTP('CORELLI', Bank='59', Tube='1-4', Pixel='65-145')
@@ -16,4 +19,8 @@ SaveMask('CORELLI', outDir+'mask.xml')
 SA, Flux = MDNormSCDPreprocessIncoherent(Filename='CORELLI_8602',
                                          MomentumMin=2.5,
                                          MomentumMax=10,
-                                         MaskFile=MaskFilename)
+                                         MaskFile=outDir+'mask.xml')
+
+
+SaveNexus(SA, outDir+'SA.nxs')
+SaveNexus(Flux, outDir+'Flux.nxs')
