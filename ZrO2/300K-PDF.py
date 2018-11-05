1e6d0e54c735bfdc60d562ee31a7fa298433ea13
Ross Whitfield <whitfieldre@ornl.gov>
2018-11-05 16:20:54 -0500

    Update ZrO2/300K-PDF.py

--- ZrO2/300K-PDF.py
+++ ZrO2/300K-PDF.py
@@ -1,18 +1,23 @@
 outDir = '/SNS/users/rwp/corelli/ZrO2/'
 
-ConvertMultipleRunsToSingleCrystalMD(Filename='CORELLI_8192:8246',SetGoniometer=True,Axis0='BL9:Mot:Sample:Axis1,0,1,0,1',OutputWorkspace='md',MinValues=[-15,-15,-15],MaxValues=[15,15,15])
-FindPeaksMD(InputWorkspace='md', PeakDistanceThreshold=0.25, DensityThresholdFactor=1000000, OutputWorkspace='peaks')
+ConvertMultipleRunsToSingleCrystalMD(Filename='CORELLI_8192:8246',
+                                     SetGoniometer=True,
+                                     Axis0='BL9:Mot:Sample:Axis1,0,1,0,1',
+                                     OutputWorkspace='md',
+                                     MinValues=[-15,-15,-15],
+                                     MaxValues=[15,15,15])
+FindPeaksMD(InputWorkspace='md', PeakDistanceThreshold=0.25, DensityThresholdFactor=10000, OutputWorkspace='peaks')
 FindUBUsingFFT(PeaksWorkspace='peaks', MinD=4, MaxD=6)
 ShowPossibleCells(PeaksWorkspace='peaks')
 SelectCellOfType(PeaksWorkspace='peaks', CellType='Orthorhombic', Centering='F', Apply=True)
 IndexPeaks(PeaksWorkspace='peaks')
+OptimizeLatticeForCellType(PeaksWorkspace='peaks', Apply=True)
 
 SaveIsawUB('peaks', outDir+'ZrO2_300K.mat')
 
-
 SingleCrystalDiffuseReduction(Filename='CORELLI_8192:8246',
-                              SolidAngle='/SNS/CORELLI/shared/Vanadium/2015A/SolidAngle20150411.nxs',
-                              Flux='/SNS/CORELLI/shared/Vanadium/2015A/Spectrum20150411.nxs',
+                              SolidAngle=outDir+'IPTS-12310/SA.nxs',
+                              Flux=outDir+'IPTS-12310/Flux.nxs',
                               OutputWorkspace='output',
                               SetGoniometer=True,
                               Axis0="BL9:Mot:Sample:Axis1,0,1,0,1",
