9fc684e64a131c9db835ea8c3643c62a98a4e3da
Ross Whitfield <whitfieldre@ornl.gov>
2018-11-08 15:19:08 -0500

    Update ZrO2/IPTS-12310/300K-PDF.py

--- ZrO2/IPTS-12310/300K-PDF.py
+++ ZrO2/IPTS-12310/300K-PDF.py
@@ -56,14 +56,23 @@ SingleCrystalDiffuseReduction(Filename='CORELLI_8569:8601',
                               SolidAngle=outDir+'SA.nxs',
                               Flux=outDir+'Flux.nxs',
                               MaskFile=outDir+'mask.xml',
-                              OutputWorkspace='output',
+                              OutputWorkspace='sym',
                               SetGoniometer=True,
                               Axis0="BL9:Mot:Sample:Axis1,0,1,0,1",
                               UBMatrix=outDir+'ZrO2_300K.mat',
                               BinningDim0='-10.01,10.01,1001',
                               BinningDim1='-10.01,10.01,1001',
-                              BinningDim2='-10.01,10.01,1001')
+                              BinningDim2='-10.01,10.01,1001',
+                              SymmetryOps='221') # Really 225
 
+SaveMD('sym', outDir+'ZrO2_300K_sym_1000.nxs')
+#sym=LoadMD(outDir+'ZrO2_300K_sym.nxs')
+
+DeltaPDF3D(InputWorkspace='sym', IntermediateWorkspace='int', OutputWorkspace='out', Method='KAREN', Convolution=False)
+SaveMD('out', outDir+'ZrO2_300K_PDF_1000.nxs')
+SaveMDWorkspaceToVTK('out', outDir+'ZrO2_300K_PDF.vts')
+
+# Double volume
 
 SingleCrystalDiffuseReduction(Filename='CORELLI_8569:8601',
                               SolidAngle=outDir+'SA.nxs',
@@ -73,12 +82,12 @@ SingleCrystalDiffuseReduction(Filename='CORELLI_8569:8601',
                               SetGoniometer=True,
                               Axis0="BL9:Mot:Sample:Axis1,0,1,0,1",
                               UBMatrix=outDir+'ZrO2_300K.mat',
-                              BinningDim0='-10.02,10.02,501',
-                              BinningDim1='-10.02,10.02,501',
-                              BinningDim2='-10.02,10.02,501',
+                              BinningDim0='-20.02,20.02,1001',
+                              BinningDim1='-20.02,20.02,1001',
+                              BinningDim2='-20.02,20.02,1001',
                               SymmetryOps='221') # Really 225
 
-SaveMD('sym', outDir+'ZrO2_300K_sym_1000.nxs')
+SaveMD('sym', outDir+'ZrO2_300K_sym_20.02.nxs')
 #sym=LoadMD(outDir+'ZrO2_300K_sym.nxs')
 
 DeltaPDF3D(InputWorkspace='sym', IntermediateWorkspace='int', OutputWorkspace='out', Method='KAREN', Convolution=False)
