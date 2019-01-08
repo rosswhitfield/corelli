d33f74b3513edc63f18096d5e0eae159d28e2376
Ross Whitfield <whitfieldre@ornl.gov>
2019-01-08 13:19:12 -0500

    Update cal_2018_10/.#cal_CsLaNb2O7.py cal_2018_10/cal_CsLaNb2O7.py

--- cal_2018_10/cal_CsLaNb2O7.py
+++ cal_2018_10/cal_CsLaNb2O7.py
@@ -9,26 +9,24 @@ def get_d(ol, h, k, l):
 
 ol=OrientedLattice(3.905044, 3.905044, 11.166629, 90, 90, 90)
 
-DReference = [get_d(ol, 0, 0, 1), # 11.166629
-              #get_d(ol, 0, 0, 2), # 5.5833145
-              get_d(ol, 1, 0, 0), # 3.9050440
-              #get_d(ol, 0, 0, 3), # 3.7222097
-              #get_d(ol, 1, 0, 1), # 3.6861455
-              get_d(ol, 1, 0, 2), # 3.2000186
-              # 2.79165725
-              get_d(ol, 1, 1, 0), # 2.7612831
-              #get_d(ol, 1, 0, 3), # 2.6943178
-              #get_d(ol, 1, 1, 1), # 2.6805448
-              #get_d(ol, 1, 1, 2), # 2.4751292 maybe
-              # 2.27102109
-              # 2.2333258
-              # 2.2176826
-              # 1.96317278
-              # 1.952522
-              # 1.93866933
-              # 1.92334155
-              # 1.86110483
-              # 1.84307275
+DReference = [get_d(ol, 0, 0, 1),
+              get_d(ol, 0, 0, 2),
+              get_d(ol, 1, 0, 0),
+              get_d(ol, 0, 0, 3),
+              get_d(ol, 1, 0, 1),
+              get_d(ol, 1, 0, 2),
+              get_d(ol, 0, 0, 4),
+              get_d(ol, 1, 1, 0),
+              get_d(ol, 1, 0, 3),
+              get_d(ol, 1, 1, 1),
+              get_d(ol, 1, 1, 2),
+              get_d(ol, 1, 1, 3),
+
+
+
+
+
+              get_d(ol, 2, 0, 6),
 ]
 
 DReference = [get_d(ol, 0, 0, 1), # 11.166629
@@ -47,6 +45,10 @@ ApplyCalibration('raw','CalibTable')
 
 #ConvertUnits(InputWorkspace='raw', OutputWorkspace='rawD', Target='dSpacing')
 
+corelli=LoadEmptyInstrument(InstrumentName='CORELLI')
+ApplyCalibration('corelli','CalibTable')
+difc0=CalculateDIFC('corelli')
+
 
 #LoadIsawDetCal('raw','/SNS/users/rwp/corelli/cal_2018_05/0a/Aligned_row_si_c60.nxs.detcal')
 
@@ -62,6 +64,9 @@ PDCalibration(InputWorkspace='raw',
               OutputCalibrationTable='cal',
               DiagnosticWorkspaces='diag')
 
+difc=CalculateDIFC('corelli',CalibrationWorkspace='cal')
+diff = difc/difc0
+
 SaveDiffCal('cal',MaskWorkspace='cal_mask', Filename='/SNS/users/rwp/corelli/cal_2018_10/cal.h5')
 
 PDCalibration(InputWorkspace='raw',
@@ -74,12 +79,8 @@ PDCalibration(InputWorkspace='raw',
               OutputCalibrationTable='cal1',
               DiagnosticWorkspaces='diag1')
 
+difc1=CalculateDIFC('corelli',CalibrationWorkspace='cal1')
+diff1 = difc1/difc0
+
 SaveDiffCal('cal1',MaskWorkspace='cal1_mask', Filename='/SNS/users/rwp/corelli/cal_2018_10/cal1.h5')
 
-corelli=LoadEmptyInstrument(InstrumentName='CORELLI')
-ApplyCalibration('corelli','CalibTable')
-difc0=CalculateDIFC('corelli')
-
-difc=CalculateDIFC('corelli',CalibrationWorkspace='cal')
-
-diff = difc/difc0
