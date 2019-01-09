a7bb7c6fdbf6c2bb1bf0ec53ecd0da9dba75445b
Ross Whitfield <whitfieldre@ornl.gov>
2019-01-09 11:19:13 -0500

    Update cal_2018_10_Natrolite/try_some_data.py

---
+++ cal_2018_10_Natrolite/try_some_data.py
@@ -0,0 +1,2 @@
+Load('CORELLI_81285', OutputWorkspace='CORELLI_81285')
+ConvertToMD(InputWorkspace='CORELLI_81285', QDimensions='Q3D', dEAnalysisMode='Elastic', Q3DFrames='Q_sample', OutputWorkspace='md')
