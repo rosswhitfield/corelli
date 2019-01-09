39121b733d7baa3cc75d9d811b409e000d2dd37c
Ross Whitfield <whitfieldre@ornl.gov>
2019-01-09 12:19:11 -0500

    Update cal_2018_10_Natrolite/try_some_data.py

--- cal_2018_10_Natrolite/try_some_data.py
+++ cal_2018_10_Natrolite/try_some_data.py
@@ -1,2 +1,26 @@
 Load('CORELLI_81285', OutputWorkspace='CORELLI_81285')
-ConvertToMD(InputWorkspace='CORELLI_81285', QDimensions='Q3D', dEAnalysisMode='Elastic', Q3DFrames='Q_sample', OutputWorkspace='md')
+SetGoniometer(Workspace='CORELLI_81285', Axis0='BL9:Mot:Sample:Axis3,0,1,0,1')
+ConvertToMD(InputWorkspace='CORELLI_81285', QDimensions='Q3D', dEAnalysisMode='Elastic', Q3DFrames='Q_sample', OutputWorkspace='md_81285')
+
+Load('CORELLI_81300', OutputWorkspace='CORELLI_81300')
+SetGoniometer(Workspace='CORELLI_81300', Axis0='BL9:Mot:Sample:Axis3,0,1,0,1')
+ConvertToMD(InputWorkspace='CORELLI_81300', QDimensions='Q3D', dEAnalysisMode='Elastic', Q3DFrames='Q_sample', OutputWorkspace='md_81300')
+
+
+
+ConvertMultipleRunsToSingleCrystalMD(Filename='CORELLI_81285:81433:5',
+                                     FilterByTofMin=1000,
+                                     FilterByTofMax=16666,
+                                     MinValues=[-15,-5,-15],
+                                     MaxValues=[15,5,15],
+                                     OutputWorkspace='output',
+                                     SetGoniometer=True,
+                                     Axis0="BL9:Mot:Sample:Axis3,0,1,0,1")
+FindPeaksMD(InputWorkspace='output', PeakDistanceThreshold=0.5, MaxPeaks=2000, OutputWorkspace='peaks')
+FindUBUsingFFT(PeaksWorkspace='peaks', MinD=5, MaxD=20)
+ShowPossibleCells(PeaksWorkspace='peaks')
+SelectCellWithForm(PeaksWorkspace='peaks', FormNumber=26, Apply=True)
+IndexPeaks(PeaksWorkspace='peaks')
+OptimizeLatticeForCellType(PeaksWorkspace='peaks', CellType='Orthorhombic', Apply=True, OutputDirectory='/SNS/users/rwp/.')
+
+# Lattice Parameters:    6.703017   18.304804   18.669009   90.000000   90.000000   90.000000  2290.639063
