ecf386b66a560bb78849b46ecaf49ccab552ed61
Ross Whitfield <whitfieldre@ornl.gov>
2019-01-10 11:19:12 -0500

    Update cal_2018_10_Natrolite/try_some_data_with_tube_cal.py

--- cal_2018_10_Natrolite/try_some_data_with_tube_cal.py
+++ cal_2018_10_Natrolite/try_some_data_with_tube_cal.py
@@ -1,22 +1,30 @@
-from mantid import config
-config['loading.multifilelimit']='1000'
+import tube
+tube.readCalibrationFile('CalibTable','/SNS/users/rwp/corelli/tube_calibration2/CalibTable2_combined.txt')
 
+for run in range(81285,81405+1,10):
+    Load(Filename='CORELLI_{}'.format(run),
+         OutputWorkspace='run',
+         FilterByTofMin=1000,
+         FilterByTofMax=16666)
+    SetGoniometer(Workspace='run',
+                  Axis0='BL9:Mot:Sample:Axis3,0,1,0,1')
+    ApplyCalibration('run','CalibTable')
+    ConvertToMD(InputWorkspace='run',
+                OutputWorkspace='output',
+                QDimensions='Q3D',
+                dEAnalysisMode='Elastic',
+                Q3DFrames='Q_sample',
+                MinValues=[-15,-5,-15],
+                MaxValues=[15,5,15],
+                OverwriteExisting=False)
 
-for run in range(81285,81405+1):
-
-ConvertMultipleRunsToSingleCrystalMD(Filename='CORELLI_81285:81405:5',
-                                     FilterByTofMin=1000,
-                                     FilterByTofMax=16666,
-                                     MinValues=[-15,-5,-15],
-                                     MaxValues=[15,5,15],
-                                     OutputWorkspace='output',
-                                     SetGoniometer=True,
-                                     Axis0="BL9:Mot:Sample:Axis3,0,1,0,1")
 FindPeaksMD(InputWorkspace='output', PeakDistanceThreshold=0.5, MaxPeaks=2000, OutputWorkspace='peaks')
 FindUBUsingFFT(PeaksWorkspace='peaks', MinD=5, MaxD=20)
 ShowPossibleCells(PeaksWorkspace='peaks')
 SelectCellWithForm(PeaksWorkspace='peaks', FormNumber=26, Apply=True)
 IndexPeaks(PeaksWorkspace='peaks')
+
+
 SaveIsawPeaks('peaks', Filename='/SNS/users/rwp/corelli/cal_2018_10_Natrolite/peaks_5.peaks')
 OptimizeLatticeForCellType(PeaksWorkspace='peaks', CellType='Orthorhombic', Apply=True, OutputDirectory='/SNS/users/rwp/.')
 SaveIsawPeaks('peaks', Filename='/SNS/users/rwp/corelli/cal_2018_10_Natrolite/peaks_5_opt.peaks')
