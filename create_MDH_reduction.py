f32a9982e292e555745fefd4ba814392dc25f8b5
Ross Whitfield <whitfieldre@ornl.gov>
2019-01-16 14:19:09 -0500

    Update .#create_MDH_reduction.py create_MDH_reduction.py

--- create_MDH_reduction.py
+++ create_MDH_reduction.py
@@ -1,7 +1,10 @@
+#from mantid.simpleapi import LoadMD
 import h5py
 import json
 import sys
-import re
+import getpass
+import datetime
+import os
 
 output_file = sys.argv[1]
 
@@ -10,7 +13,18 @@ f = h5py.File(output_file, 'r')
 output = {}
 
 output['output_files'] = [{'location':output_file, 'type':'MDHistoWorkspace'}]
+output['user'] = getpass.getuser()
+output['created'] = datetime.datetime.fromtimestamp(os.path.getmtime('/SNS/CORELLI/IPTS-15526/shared/Normalized_300K_noCC_Simple.nxs')).isoformat()
 
 history = f['/MDHistoWorkspace/process/MantidAlgorithm_1/data']
 
-output['input_files'] = 
+output['input_files'] = []
+
+for hist in history:
+    for prop in hist.decode().split('Name: '):
+        for part in prop.split(', '):
+            if part[0] == 'SolidAngle' or part[0] == 'Flux':
+                output['input_files'].append({'location':part[1], 'type':'type'})
+
+print(json.dumps(output))
+
