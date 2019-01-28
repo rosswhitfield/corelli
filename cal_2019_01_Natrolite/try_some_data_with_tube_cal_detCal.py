7217b944331cc01ef93b3db41a1c2f34fa368356
Ross Whitfield <whitfieldre@ornl.gov>
2019-01-28 14:19:10 -0500

    Update cal_2019_01_Natrolite/try_some_data_with_tube_cal.py cal_2019_01_Natrolite/try_some_data_with_tube_cal_detCal.py

--- cal_2019_01_Natrolite/try_some_data_with_tube_cal_detCal.py
+++ cal_2019_01_Natrolite/try_some_data_with_tube_cal_detCal.py
@@ -1,3 +1,4 @@
+from mantid.simpleapi import *
 import tube
 tube.readCalibrationFile('CalibTable','/SNS/users/rwp/corelli/tube_calibration2/CalibTable2_combined.txt')
 
@@ -7,6 +8,7 @@ for run in range(83349,83432+1):
          FilterByTofMin=1000,
          FilterByTofMax=16666)
     ApplyCalibration('run','CalibTable')
+    LoadIsawDetCal('run','/SNS/users/rwp/corelli/cal_2018_05/a/Aligned3.nxs.detcal')
     ConvertToMD(InputWorkspace='run',
                 OutputWorkspace='output',
                 QDimensions='Q3D',
@@ -16,21 +18,16 @@ for run in range(83349,83432+1):
                 MaxValues=[15,5,15],
                 OverwriteExisting=False)
                 
-SaveMD('output', '/SNS/CORELLI/IPTS-21655/shared/rwp/CORELLI_83349_83432_tubecal_MDE.nxs')
+SaveMD('output', '/SNS/CORELLI/IPTS-21655/shared/rwp/CORELLI_83349_83432_tubecal_detcal_MDE.nxs')
 #output=LoadMD('/SNS/CORELLI/IPTS-21655/shared/rwp/CORELLI_83349_83432_tubecal_MDE.nxs')
 
+
+"""
 FindPeaksMD(InputWorkspace='output', PeakDistanceThreshold=0.5, MaxPeaks=2000, OutputWorkspace='peaks')
 FindUBUsingFFT(PeaksWorkspace='peaks', MinD=5, MaxD=20)
 ShowPossibleCells(PeaksWorkspace='peaks')
 SelectCellWithForm(PeaksWorkspace='peaks', FormNumber=26, Apply=True)
 IndexPeaks(PeaksWorkspace='peaks')
-
-# 6.684847   18.301256   18.670389   90.003998   90.119247   89.780933
-
-OptimizeLatticeForCellType(PeaksWorkspace='peaks', CellType='Orthorhombic', Apply=True, OutputDirectory='/SNS/users/rwp/.')
-
-# 6.684953   18.316274   18.654818   90.000000   90.000000   90.000000 
-
 
 
 SCDCalibratePanels(PeakWorkspace='peaks', a=6.56, b=18.27, c=18.587, alpha=90, beta=90, gamma=90, ChangeL1=False)
@@ -39,3 +36,4 @@ SCDCalibratePanels(PeakWorkspace='peaks', a=6.56, b=18.27, c=18.587, alpha=90, b
 
 
 SaveIsawPeaks('peaks', Filename='/SNS/users/rwp/corelli/cal_2019_01_Natrolite/peaks_tubeCal.peaks')
+"""
