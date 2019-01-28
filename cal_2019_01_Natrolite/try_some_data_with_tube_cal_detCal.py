6b97afbbc703aa0a7ee26e7b986522e8f06bd2f9
Ross Whitfield <whitfieldre@ornl.gov>
2019-01-28 18:19:08 -0500

    Update cal_2019_01_Natrolite/try_some_data_with_tube_cal_detCal.py

--- cal_2019_01_Natrolite/try_some_data_with_tube_cal_detCal.py
+++ cal_2019_01_Natrolite/try_some_data_with_tube_cal_detCal.py
@@ -19,7 +19,7 @@ for run in range(83349,83432+1):
                 OverwriteExisting=False)
                 
 SaveMD('output', '/SNS/CORELLI/IPTS-21655/shared/rwp/CORELLI_83349_83432_tubecal_detcal_MDE.nxs')
-#output=LoadMD('/SNS/CORELLI/IPTS-21655/shared/rwp/CORELLI_83349_83432_tubecal_MDE.nxs')
+#output=LoadMD('/SNS/CORELLI/IPTS-21655/shared/rwp/CORELLI_83349_83432_tubecal_detcal_MDE.nxs')
 
 
 
@@ -28,6 +28,14 @@ FindUBUsingFFT(PeaksWorkspace='peaks', MinD=5, MaxD=20)
 ShowPossibleCells(PeaksWorkspace='peaks')
 SelectCellWithForm(PeaksWorkspace='peaks', FormNumber=26, Apply=True)
 IndexPeaks(PeaksWorkspace='peaks')
+
+# 6.684847   18.301256   18.670389   90.003998   90.119247   89.780933
+
+OptimizeLatticeForCellType(PeaksWorkspace='peaks', CellType='Orthorhombic', Apply=True, OutputDirectory='/SNS/users/rwp/.')
+
+# 6.684953   18.316274   18.654818   90.000000   90.000000   90.000000 
+
+
 
 
 SCDCalibratePanels(PeakWorkspace='peaks', a=6.56, b=18.27, c=18.587, alpha=90, beta=90, gamma=90, ChangeL1=False)
