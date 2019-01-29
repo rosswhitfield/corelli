4a886a8d3f4f59fabfa06405e3e2df712d7d211d
Ross Whitfield <whitfieldre@ornl.gov>
2019-01-29 08:19:09 -0500

    Update cal_2019_01_Natrolite/try_some_data_with_tube_cal_detCal.py

--- cal_2019_01_Natrolite/try_some_data_with_tube_cal_detCal.py
+++ cal_2019_01_Natrolite/try_some_data_with_tube_cal_detCal.py
@@ -29,18 +29,20 @@ ShowPossibleCells(PeaksWorkspace='peaks')
 SelectCellWithForm(PeaksWorkspace='peaks', FormNumber=26, Apply=True)
 IndexPeaks(PeaksWorkspace='peaks')
 
-# 6.684847   18.301256   18.670389   90.003998   90.119247   89.780933
+# 6.684847   18.301256   18.670389   90.003998   90.119247   89.780933 # without detCal
+# 6.625729   18.269036   18.640500   89.992889   89.956041   89.853691
 
-OptimizeLatticeForCellType(PeaksWorkspace='peaks', CellType='Orthorhombic', Apply=True, OutputDirectory='/SNS/users/rwp/.')
+#OptimizeLatticeForCellType(PeaksWorkspace='peaks', CellType='Orthorhombic', Apply=True, OutputDirectory='/SNS/users/rwp/.')
 
-# 6.684953   18.316274   18.654818   90.000000   90.000000   90.000000 
+# 6.684953   18.316274   18.654818   90.000000   90.000000   90.000000 # without detCal
 
+# Try
+# 18.2930 18.6430 6.5860 90 90 90
+# 18.3720 18.5760 6.606 90 90 90
 
+#SCDCalibratePanels(PeakWorkspace='peaks', a=6.56, b=18.27, c=18.587, alpha=90, beta=90, gamma=90, ChangeL1=False)
 
-
-SCDCalibratePanels(PeakWorkspace='peaks', a=6.56, b=18.27, c=18.587, alpha=90, beta=90, gamma=90, ChangeL1=False)
-
-SCDCalibratePanels(PeakWorkspace='peaks', a=6.56, b=18.27, c=18.587, alpha=90, beta=90, gamma=90, ChangeL1=False, DetCalFilename='/SNS/users/rwp/SCDCalibrate.DetCal', ColFilename='/SNS/users/rwp/ColCalcvsTheor.nxs', RowFilename='/SNS/users/rwp/RowCalcvsTheor.nxs', TofFilename='/SNS/users/rwp/TofCalcvsTheor.nxs')
+SCDCalibratePanels(PeakWorkspace='peaks', a=6.586, b=18.2930, c=18.643, alpha=90, beta=90, gamma=90, ChangeL1=False, DetCalFilename='/SNS/users/rwp/SCDCalibrate.DetCal', ColFilename='/SNS/users/rwp/ColCalcvsTheor.nxs', RowFilename='/SNS/users/rwp/RowCalcvsTheor.nxs', TofFilename='/SNS/users/rwp/TofCalcvsTheor.nxs')
 
 
 SaveIsawPeaks('peaks', Filename='/SNS/users/rwp/corelli/cal_2019_01_Natrolite/peaks_tubeCal_detCal.peaks')
