15ba1b2d5c2265eac76ad44d6400c8ca8fdcf409
Ross Whitfield <whitfieldre@ornl.gov>
2019-03-04 17:19:08 -0500

    Update create_MDH_reduction.py

--- create_MDH_reduction.py
+++ create_MDH_reduction.py
@@ -1,11 +1,9 @@
 from mantid.simpleapi import LoadMD
 from mantid.kernel import version_str
-import h5py
 import json
 import sys
 import getpass
 import datetime
-import os
 
 output_file = sys.argv[1]
 
@@ -16,28 +14,29 @@ if not md.isMDHistoWorkspace():
 
 output = {}
 
-output['output_files'] = [{'location':output_file, 'type':'processed'}]
+output['output_files'] = [{'location': output_file,
+                           'type': 'processed',
+                           'purpose': 'reduced-data'}]
 output['user'] = getpass.getuser()
 output['created'] = datetime.datetime.now().replace(microsecond=0).isoformat()
 
 output['input_files'] = []
+
 
 def get_children_algs(history, result):
     if history.childHistorySize() == 0:
         result.append(history)
     else:
         for histories in history.getChildHistories():
-            get_children_algs(histories,result)
+            get_children_algs(histories, result)
+
 
 history = md.getHistory()
 for hist in history.getAlgorithmHistories()[:-1]:
     algs = []
     get_children_algs(hist, algs)
-    print(algs)
     for alg in algs:
-        print(alg.name())
         if 'Load' in alg.name():
-            print(alg.name())
             file_type = 'user-provided'
             if alg.name() in ["Load", "LoadEventNexus", "LoadWANDSCD"]:
                 file_type = 'raw'
@@ -45,24 +44,26 @@ for hist in history.getAlgorithmHistories()[:-1]:
                 file_type = 'processed'
             for prop in alg.getProperties():
                 if "Filename" in prop.name():
-                    output['input_files'].append({'location':prop.value(), 'type':file_type})
+                    output['input_files'].append({'location': prop.value(),
+                                                  'type': file_type,
+                                                  'purpose': 'sample-data'})
 
 metadata = {}
 
 metadata['dimensions'] = []
 for ndim in range(md.getNumDims()):
     dim = md.getDimension(ndim)
-    metadata['dimensions'].append({'id':dim.getDimensionId(),
-                                   'name':dim.getName(),
-                                   'units':dim.getUnits(),
-                                   'minimum':dim.getMinimum(),
-                                   'maximum':dim.getMaximum(),
-                                   'number_of_bins':dim.getNBins(),
-                                   'bin_width':dim.getBinWidth()})
+    metadata['dimensions'].append({'id': dim.getDimensionId(),
+                                   'name': dim.getName(),
+                                   'units': dim.getUnits(),
+                                   'minimum': dim.getMinimum(),
+                                   'maximum': dim.getMaximum(),
+                                   'number_of_bins': dim.getNBins(),
+                                   'bin_width': dim.getBinWidth()})
 
 sample = {}
 sample['name'] = md.getExperimentInfo(0).sample().getName()
-ol=md.getExperimentInfo(0).sample().getOrientedLattice()
+ol = md.getExperimentInfo(0).sample().getOrientedLattice()
 sample['a'] = ol.a()
 sample['b'] = ol.b()
 sample['c'] = ol.c()
@@ -77,4 +78,3 @@ output['metadata'] = metadata
 output['mantid_version'] = version_str()
 
 print(json.dumps(output))
-
