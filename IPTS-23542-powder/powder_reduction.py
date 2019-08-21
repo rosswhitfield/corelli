843b1927952cb59b013763d6b6d150ab4ec8486f
Ross Whitfield <whitfieldre@ornl.gov>
2019-08-21 16:19:04 -0400

    Update IPTS-23542-powder/powder_reduction.py IPTS-23542-powder/powder_reduction2.py

--- IPTS-23542-powder/powder_reduction.py
+++ IPTS-23542-powder/powder_reduction.py
@@ -1,6 +1,6 @@
 SNSPowderReduction(Filename="CORELLI_101847",
                            PreserveEvents=True,
                            VanadiumNumber=-1,
-                           CalibrationFile="/SNS/CORELLI/IPTS-20253/shared/mantid_reduce/alignment/alignment_diamond_d56271.nxs",
+                           CalibrationFile="/SNS/CORELLI/IPTS-20253/shared/mantid_reduce/alignment/output/diamond_cc_off_d56271.nxs",
                            Binning='0.1,-0.002,15',
                            OutputDirectory="/SNS/users/rwp/corelli/IPTS-23542-powder/")
--- IPTS-23542-powder/powder_reduction2.py
+++ IPTS-23542-powder/powder_reduction2.py
@@ -1,6 +1,11 @@
 SNSPowderReduction(Filename="CORELLI_101847",
                            PreserveEvents=True,
+                           BackgroundNumber=-1,
                            VanadiumNumber=-1,
-                           CalibrationFile="/SNS/CORELLI/IPTS-20253/shared/mantid_reduce/alignment/alignment_diamond_d56271.nxs",
+                           VanadiumBackgroundNumber=-1,
+                           CalibrationFile="/SNS/CORELLI/IPTS-22673/shared/mantid_reduce/calibration/CORELLI_d56271_2017_11_17_CCR.h5",
                            Binning='0.1,-0.002,15',
+                           StripVanadiumPeaks=False,
+                           GroupingFile="/SNS/users/rwp/corelli/IPTS-23542-powder/group_bank.xml",
+                           MaxChunkSize=1,
                            OutputDirectory="/SNS/users/rwp/corelli/IPTS-23542-powder/")
