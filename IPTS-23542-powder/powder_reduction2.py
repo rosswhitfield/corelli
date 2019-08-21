7a413a14891fb10d51d4db8c00458f5e1d292e2d
Ross Whitfield <whitfieldre@ornl.gov>
2019-08-21 15:19:07 -0400

    Update IPTS-23542-powder/powder_reduction.py IPTS-23542-powder/powder_reduction2.py

---
+++ IPTS-23542-powder/powder_reduction2.py
@@ -0,0 +1,6 @@
+SNSPowderReduction(Filename="CORELLI_101847",
+                           PreserveEvents=True,
+                           VanadiumNumber=-1,
+                           CalibrationFile="/SNS/CORELLI/IPTS-20253/shared/mantid_reduce/alignment/alignment_diamond_d56271.nxs",
+                           Binning='0.1,-0.002,15',
+                           OutputDirectory="/SNS/users/rwp/corelli/IPTS-23542-powder/")
