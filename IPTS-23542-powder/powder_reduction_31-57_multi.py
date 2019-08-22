3407533affbc0f5630f09913249d1936ca34a322
Ross Whitfield <whitfieldre@ornl.gov>
2019-08-22 12:19:14 -0400

    Update IPTS-23542-powder/powder_reduction_31-57_multi.py

---
+++ IPTS-23542-powder/powder_reduction_31-57_multi.py
@@ -0,0 +1,15 @@
+from mantid.simpleapi import *
+
+SNSPowderReduction(Filename="CORELLI_101847",
+                   BackgroundNumber=-1,
+                   VanadiumNumber=96959,
+                   VanadiumBackgroundNumber=-1,
+                   CalibrationFile="/SNS/CORELLI/IPTS-22673/shared/mantid_reduce/calibration/CORELLI_d56271_2017_11_17_CCR.h5",
+                   Binning='0.1,0.002,15',
+                   CropWavelengthMin=0.7,
+                   CropWavelengthMax=2.5,
+                   StripVanadiumPeaks=False,
+                   GroupingFile="/SNS/users/rwp/corelli/IPTS-23542-powder/group_31-57.xml",
+                   OutputFilePrefix="31-57_",
+                   SaveAs='fullprof,nexus',
+                   OutputDirectory="/SNS/users/rwp/corelli/IPTS-23542-powder/")
