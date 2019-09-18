91277d3d3484c515e647d332dff37c17ab60a307
Ross Whitfield <whitfieldre@ornl.gov>
2019-09-18 13:19:05 -0400

    Update tdc_analysis/plot_tdc_period_h5py.py tdc_analysis/plot_tdc_period_h5py_glob.py

--- tdc_analysis/plot_tdc_period_h5py.py
+++ tdc_analysis/plot_tdc_period_h5py.py
@@ -4,14 +4,12 @@ import h5py
 import numpy as np
 import matplotlib.pyplot as plt
 
+filename = '/SNS/CORELLI/IPTS-23139/nexus/CORELLI_103083.nxs.h5'
+filename = '/SNS/CORELLI/IPTS-20534/nexus/CORELLI_82222.nxs.h5'
 
-#ws = LoadEventNexus('CORELLI_103083',MetaDataOnly=True)
-f = h5py.File('/SNS/CORELLI/IPTS-23139/nexus/CORELLI_103083.nxs.h5', 'r')
-#tdc = ws.getRun().getProperty("chopper4_TDC").times
+f = h5py.File(filename, 'r')
 tdc = f['entry/DASlogs/chopper4_TDC/value'].value.astype(np.int64)
-#wl =  ws.getRun().getProperty("BL9:Chop:Skf4:WavelengthSet").value[0]
 wl = f['entry/DASlogs/BL9:Chop:Skf4:WavelengthSet/value'].value[0]
-#phaseDelay =  ws.getRun().getProperty("BL9:Chop:Skf4:PhaseTimeDelaySet").value[0]
 phaseDelay = f['entry/DASlogs/BL9:Chop:Skf4:PhaseTimeDelaySet/value'].value[0]
 
 diff = (tdc[1:] - tdc[:-1])/100
@@ -23,4 +21,6 @@ unique, unique_counts = np.unique(diff, return_counts=True)
 plt.plot(unique, unique_counts)
 plt.xlabel("Chopper period (100ns)")
 plt.ylabel("Period frequency")
+plt.title("{} - wavelength = {} - Phase Delay = {}".format(filename.split('/')[-1],wl, spaseDelay))
+plt.xlim(34060, 34100)
 plt.show()
