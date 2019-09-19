b1261bf0a05c27577e8c2224310cdc350fabb52e
Ross Whitfield <whitfieldre@ornl.gov>
2019-09-19 11:19:08 -0400

    Update tdc_analysis/plot_tdc_period_h5py.py

--- tdc_analysis/plot_tdc_period_h5py.py
+++ tdc_analysis/plot_tdc_period_h5py.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 
 filename = '/SNS/CORELLI/IPTS-23139/nexus/CORELLI_103083.nxs.h5'
 filename = '/SNS/CORELLI/IPTS-20534/nexus/CORELLI_82222.nxs.h5'
+filename = '/SNS/CORELLI/IPTS-23139/nexus/CORELLI_103272.nxs.h5'
 
 f = h5py.File(filename, 'r')
 tdc = f['entry/DASlogs/chopper4_TDC/value'].value.astype(np.int64)
@@ -21,6 +22,6 @@ unique, unique_counts = np.unique(diff, return_counts=True)
 plt.plot(unique, unique_counts)
 plt.xlabel("Chopper period (100ns)")
 plt.ylabel("Period frequency")
-plt.title("{} - wavelength = {} - Phase Delay = {}".format(filename.split('/')[-1],wl, spaseDelay))
-plt.xlim(34060, 34100)
+plt.title("{} - wavelength = {} - Phase Delay = {}".format(filename.split('/')[-1],wl, phaseDelay))
+#plt.xlim(34060, 34100)
 plt.show()
